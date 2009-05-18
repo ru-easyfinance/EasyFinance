2f4d95dc4ac5f185b2c4476602a877a88fc7403c
ukko <ukko@240b7755-b59a-4df0-b20f-7dab6991996d>
2009-05-18 10:02:55 +0000

    Перемещаем внешние зависимости в ядро проекта
    git-svn-id: svn://hm.podzone.net/branches/old@14 240b7755-b59a-4df0-b20f-7dab6991996d

--- www/index.php
+++ www/index.php
@@ -1,60 +1,58 @@
 <?php
 /**
-* file: index.php
-* author: Roman Korostov
-* date: 23/01/07	
-**/
+ * Индексный файл проекта
+ * SVN $Id$
+ */
 
 if ($_GET['et']=='on') {
+    //FIXME Редиректить на красивую страничку
     die("На сайте проводятся технические работы. Зайдите позже.");
 }
 
 global $modules;
 
-require_once ("../include/common.hm.php");
+require_once dirname(dirname(__FILE__)). "/include/common.hm.php";
 
 //если в гете пришел модуль
 if (!empty($g_modules))
 {
-	//проверим его
-	$is_module = html($g_modules);
-	
-	//если модуль проверен и существует, то загружаем его
-	if (isset($is_module) && $is_module != "")
-	{
-		if (checkModules($is_module))
-		{
-			require_once (SYS_DIR_MOD."/".$is_module.".php");
-		}
-		else
-		{
-			message_error(GENERAL_ERROR, "ModFile '".$is_module."' is not exists!");
-		}
-	}
-	else
-	{
-		message_error(GENERAL_ERROR, "ModFile '".$is_module."' is not exists!");
-	}
+    //проверим его
+    $is_module = html($g_modules);
+
+    //если модуль проверен и существует, то загружаем его
+    if (isset($is_module) && $is_module != "")
+    {
+        if (checkModules($is_module))
+        {
+            require_once (SYS_DIR_MOD."/".$is_module.".php");
+        }
+        else
+        {
+            message_error(GENERAL_ERROR, "ModFile '".$is_module."' is not exists!");
+        }
+    }
+    else
+    {
+        message_error(GENERAL_ERROR, "ModFile '".$is_module."' is not exists!");
+    }
 }else{
-	if (checkModules(DEFAULT_MODULE))
-	{
-		require_once (SYS_DIR_MOD."/".DEFAULT_MODULE.".php");
-	}
-	else
-	{
-		message_error(GENERAL_ERROR, "ModFile '".DEFAULT_MODULE."' is not exists!");
-	}
+    if (checkModules(DEFAULT_MODULE))
+    {
+        require_once (SYS_DIR_MOD."/".DEFAULT_MODULE.".php");
+    }
+    else
+    {
+        message_error(GENERAL_ERROR, "ModFile '".DEFAULT_MODULE."' is not exists!");
+    }
 }
 
 if (!empty($_SESSION['user']))
 {
-	$tpl->assign("user", $_SESSION['user']);
-	$tpl->display("index.hm.html");
+    $tpl->assign("user", $_SESSION['user']);
+    $tpl->display("index.hm.html");
 }
 else{
-	$tpl->display("index.hm.html");
-	//$tpl->display("auth2.html");
-	//$tpl->display("welcam.html");
+    $tpl->display("index.hm.html");
 }
 
 ?>
