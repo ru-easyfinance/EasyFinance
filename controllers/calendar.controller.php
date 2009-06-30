23df9a390b484c12ef04d2a5e0d9384c429e1cea
ukko <ukko@240b7755-b59a-4df0-b20f-7dab6991996d>
2009-06-30 07:10:27 +0000

    Некоторые новые изменения
    git-svn-id: svn://hm.podzone.net/branches/old@59 240b7755-b59a-4df0-b20f-7dab6991996d

--- controllers/calendar.controller.php
+++ controllers/calendar.controller.php
@@ -29,4 +29,100 @@ class Calendar_Controller extends Template_Controller
     {
 
     }
+
+    /**
+     * Возвращает список событий, в формате JSON
+     * @return void
+     */
+    function events($args) {
+// $_GET['start'];  $_GET['end'];
+        die (json_encode(
+            array(
+                array(
+                    id => 1,
+                    key => 34345,
+                    title => "Длинное событие",
+                    dt => mktime(0,0,0,6,6),
+                    start => mktime(14,0,0,6,6),
+                    end => mktime(14,0,0,6,11),
+                    className => 'yellow',
+                    comment => "Описание этого длинного события на русском языке",
+                    draggable => true
+                ),
+                array(
+                    id => 2,
+                    title => "Повторяющееся событие",
+                    dt => mktime(0,0,0,6,2),
+                    start => mktime(14,0,0,6,2),
+                    comment => 'Описание',
+                    draggable => true
+                ),
+                array(
+                    id => 2,
+                    title => "Повторяющееся событие",
+                    dt => mktime(0,0,0,6,9),
+                    start => mktime(14,0,0,6,9),
+                    draggable => true
+                ),
+                array(
+                    id => 2,
+                    title => "Повторяющееся событие",
+                    dt => mktime(0,0,0,6,16),
+                    start => mktime(14,0,0,6,16),
+                    draggable => true
+                ),
+                array(
+                    id => 3,
+                    title => "Общение",
+                    dt => mktime(0,0,0,6,20),
+                    start => mktime(9,0,0,6,20),
+                    className => 'yellow',
+                    draggable => true
+                ),
+                array(
+                    id => 4,
+                    title => "+75 000.00 руб",
+                    dt => mktime(0,0,0,6,27),
+                    start => mktime(0,0,0,6,27),
+                    className => 'green' ,
+                    showTime => false,
+                ),
+                array(
+                    id => 5,
+                    title => "-20 000.00 руб",
+                    dt => mktime(0,0,0,6,27),
+                    start => mktime(0,0,0,6,27),
+                    className => 'red',
+                    showTime => false
+                ),
+                array(
+                    id => 6,
+                    title => "30 000.00 руб",
+                    dt => mktime(0,0,0,6,27),
+                    start => mktime(0,0,0,6,27),
+                    className => 'blue',
+                    showTime => false
+                ),
+                array(
+                    id => 7,
+                    title => "Ссылка на яндекс",
+                    dt => mktime(0,0,0,6,27),
+                    start => mktime(16,0,0,6,27),
+                    className => array('yellow','url'),
+                    url => "http://yandex.ru/",
+                    showTime => false,
+                    draggable => true
+                ),
+                array(
+                    id => 8,
+                    title => "Напоминалка",
+                    dt => mktime(0,0,0,6,27),
+                    start => mktime(0,0,0,6,27),
+                    className => 'yellow',
+                    draggable => true,
+                    comment => "sdfsdf"
+                )
+            )
+        ));
+    }
 }
