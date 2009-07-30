355dfded26738ecfb1de8a49d544992fbd2667f8
ukko <ukko@240b7755-b59a-4df0-b20f-7dab6991996d>
2009-07-30 07:22:57 +0000

    Поправил операции, новый формат базы данных.
    git-svn-id: svn://hm.podzone.net/branches/old@114 240b7755-b59a-4df0-b20f-7dab6991996d

--- models/operation.model.php
+++ models/operation.model.php
@@ -11,31 +11,31 @@ class Operation_Model {
      * Ссылка на экземпляр класса базы данных
      * @var <DbSimple_Mysql>
      */
-    private $db             = null;
+    private $db = null;
 
     /**
      * Ссылка на экземпляр класса пользователя
      * @var <User>
      */
-    private $user           = null;
+    private $user = null;
 
     /**
      *
      * @var <type>
      */
-    private $account_money  = 0;
+    private $account_money = 0;
 
     /**
      *
      * @var <type>
      */
-    private $user_money     = Array();
+    private $user_money = Array();
 
     /**
      * 
      * @var <type>
      */
-    private $total_sum      = 0;
+    private $total_sum = 0;
 
     /**
      * Конструктор
@@ -93,23 +93,8 @@ class Operation_Model {
             $order1 .= " AND (cy.cat_id = {$currentCategory} OR cy.cat_parent = {$currentCategory})";
         }
 
-        $limit = "";
+        if (IS_DEMO) { //@TODO
 
-        if (IS_DEMO) {
-            $sql = "SELECT m.id, m.user_id, m.money, DATE_FORMAT(m.date,'%d.%m.%Y') as `date`,
-                       m.cat_id, m.bill_id, c.cat_name, c.cat_parent, b.bill_name, m.drain, m.comment,
-                       b.bill_currency, cu.cur_name, m.transfer, m.tr_id,
-                       bt.bill_name as cat_transfer
-                    FROM money m
-                    LEFT JOIN category c on c.cat_id = m.cat_id
-                    LEFT JOIN bill b on b.bill_id = m.bill_id and b.user_id = ?
-                    LEFT JOIN bill bt on bt.bill_id = m.transfer and bt.user_id = ?
-                    LEFT JOIN currency cu on cu.cur_id = b.bill_currency
-                    WHERE m.bill_id = ?
-                           AND m.user_id = ?
-                           ".$order."
-                    ORDER BY m.`date` DESC, m.id DESC " . $limit;
-            return $this->db->select($sql, $this->user->getId(), $this->user->getId(), $currentAccount, $this->user->getId());
         }else{
             $sql = "SELECT m.id, m.user_id, m.money, DATE_FORMAT(m.date,'%d.%m.%Y') as `date`,
                     m.cat_id, m.bill_id, c.cat_name, c.cat_parent, b.bill_name, m.drain, m.comment,
@@ -132,9 +117,10 @@ class Operation_Model {
                         LEFT JOIN category cy ON cy.cat_id = tt.category_id
                     WHERE t.user_id= ? {$order1}
                 ORDER BY `date` DESC, `id` DESC";
+            die($currentAccount . ' ' . $this->user->getId());
             $a = $this->db->select($sql, $currentAccount, $this->user->getId(), $this->user->getId());
             die(var_dump($a));
-            return $a;
+            return $a; 
         }
     }
 
