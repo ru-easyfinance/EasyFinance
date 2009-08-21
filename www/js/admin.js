df2d4703a413efcd9a65a61d92eb5f26efd0d0f4
rewle <rewle@240b7755-b59a-4df0-b20f-7dab6991996d>
2009-08-21 09:31:19 +0000

    доработки по админке
    git-svn-id: svn://hm.podzone.net/branches/old@227 240b7755-b59a-4df0-b20f-7dab6991996d

--- www/js/admin.js
+++ www/js/admin.js
@@ -1,44 +1,44 @@
 $(document).ready(function(){
-        del = function (mark){
-            $('tr#'+mark).empty()
+        var del = function (mark){
+            $('tr#'+mark).hide();
         };
-        //add = function (mark){
-        //    $(mark+' table')
-        //}
+        var content = function (mark, data){
+            $(mark+' table tr').find('td').closest('tr').empty();
+            $(mark+' table').append(data);
+        };
 
         //fields
-	$('.fields #btnAdd').click(
+	$('.fields #btnAdd').live('click',
             function(){
                 save_fields();
             });
-	$('.fields #btnCancel').click(
+	$('.fields #btnCancel').live('click',
             function(){
                 location.href="/admin/accounts/";
             });
-	$('.fields .del').click(
+	$('.fields .del').live('click',
             function(){
                 del_fields($(this).closest('tr').attr('id'));
             });
 
 	function save_fields()
 	{
-		$.ajax({
-			type: "POST",
-			url: "/admin/account_save_fields/",
-			data: {
+		$.post(
+                        "/admin/account_save_fields/",
+			 {
 				field_visual_name: $('#field_visual_name').val(),
 				field_name: $('#field_name').val(),
 				field_type: 'string',
 				field_regexp: $('#field_regexp').val(),
 				field_permissions: $('#field_permissions').val(),
 				field_default_value: $('#field_default_value').val(),
-				id: $('#id').val(),
-				ajax: true
+				id: $('#id').val()
 			},
-			success: function(data) {
-				$('#data').html(data);
-			}
-		});
+                        function (data){
+			content('.fields', data)},
+                        'text'
+			
+		);
 	}
 
         function del_fields(mark)
@@ -53,37 +53,35 @@ $(document).ready(function(){
             );
         }
         //type
-        $('.type #btnAddAccount').click(
+        $('.type #btnAddAccount').live('click',
             function(){
                 save_type();
             });
-	$('.type #btnCancelAdd').click(
+	$('.type #btnCancelAdd').live('click',
             function(){
                 location.href="/admin/accounts/";
             });
-	$('.type .del').click(
+	$('.type .del').live('click',
             function(){
                 delete_type($(this).closest('tr').attr('id'));
             });
-        $('.type .edit').click(
+        $('.type .edit').live('click',
             function(){
                 update_type($(this).closest('tr'));
             });
 
 	function save_type()
 	{
-		$.ajax({
-			type: "POST",
-			url: "/admin/account_save_type/",
-			data: {
+		$.post("/admin/account_save_type/",
+			{
 				type_name: $('#type_name').val(),
-				type_id: $('#type_id').val(),
-				ajax: true
+				type_id: $('#type_id').val()
 			},
-			success: function(data) {
-				$('#dataAccounts').html(data);
-			}
-		});
+                        function (data){
+			content('.type',data)},
+                        'text'
+			
+		);
 	}
 
         function update_type(mark)
@@ -104,35 +102,34 @@ $(document).ready(function(){
             );
         }
         //type_fields
-        $('.type_fields #btnAdd').click(
+        $('.type_fields #btnAdd').live('click',
             function(){
                 save_type_fields();
             });
-	$('.type_fields #btnCancel').click(
+	$('.type_fields #btnCancel').live('click',
             function(){
                 location.href="/admin/accounts/";
             });
-	$('.type_fields .del').click(
+	$('.type_fields .del').live('click',
             function(){
                 delete_type_fields($(this).closest('tr').attr('id'));
             });
 
 	function save_type_fields()
 	{
-		$.ajax({
-			type: "POST",
-			url: "/admin/account_save_type_fields/",
-			data: {
+		$.post(
+			"/admin/account_save_type_fields/",
+			{
 				type: $('#type').val(),
 				field: $('#field').val(),
 				id: $('#id').val(),
 				ajax: true
 			},
-			success: function(data) {
-                                $
-				$('#data').html(data);
-			}
-		});
+                        function (data){
+			content('.type_fields',data)},
+                        'text'
+			
+		);
 	}
 
         function delete_type_fields(mark)
