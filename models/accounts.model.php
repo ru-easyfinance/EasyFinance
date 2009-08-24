2f656188c0c74c154704a8d361892ae8329348da
rewle <rewle@240b7755-b59a-4df0-b20f-7dab6991996d>
2009-08-24 16:40:55 +0000

    доработки по счетам.первая операция.
    git-svn-id: svn://hm.podzone.net/branches/old@258 240b7755-b59a-4df0-b20f-7dab6991996d

--- models/accounts.model.php
+++ models/accounts.model.php
@@ -259,7 +259,7 @@ class Accounts_Model
         //alert( date('d.m.Y'));
        $model = new Operation_Model();
        $model->add($data['starter_balance'], date('Y.m.d'), 0,0,
-           'Новый счёт', $data['id']);
+           'Начальный остаток', $data['id']);
     }
 
     function update_operation($data)
@@ -267,7 +267,7 @@ class Accounts_Model
         $sql = "SElECT `id` FROM operation WHERE account_id=? AND user_id=? ORDER BY `date`";
         $oid = $this->db->selectCell($sql,$data['id'],$this->user_id);
         $model = new Operation_Model();
-       $model->edit($oid,$data['starter_balance'],date('Y.m.d'),0,0,'Новый счёт', $data['id']);
+       $model->edit($oid,$data['starter_balance'],date('Y.m.d'),0,0,'Начальный остаток', $data['id']);
        $model->save();
     }
 
