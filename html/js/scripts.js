f477b98024f2866bddaf7c682d7bc6cc215fcc37
ext1 <ext1@240b7755-b59a-4df0-b20f-7dab6991996d>
2009-08-31 14:29:14 +0000

    верстка от 31.09
    git-svn-id: svn://hm.podzone.net/branches/old@326 240b7755-b59a-4df0-b20f-7dab6991996d

--- html/js/scripts.js
+++ html/js/scripts.js
@@ -24,6 +24,9 @@ $(document).ready(function(){
     $(".inside .close").click(function(){
         $("#popupaddobject").hide();
         $("#popupadd_calendar").hide();
+        $("#poputags_add").hide();
+        $("#poputags_edit").hide();
+        $(".tags_list ul li a.edit").toggleClass("edit");
     });
     /* Display or Hide Tooltip For "Add Category" Button */
     $(".addcategories a").hover(
@@ -36,10 +39,6 @@ $(document).ready(function(){
         $("#popupadd_calendar").show();
     });
 
-    /* Display Feedback Form */
-    $(".addmessage").click(function(){
-        $("#popupreport").show();
-    });
     /* Hide Feedback Form */
     $("#popupreport .but").click(function(){
         $("#popupreport").hide();
@@ -52,75 +51,57 @@ $(document).ready(function(){
         else if($(this).hasClass("add_operation_but")){$(".addoperation").hide();}
     });
 
-    /* Display or Hide "Add New Category" Form */
-    $(".addcategories a, .cont .edit a, .cont .add a").click(function(){
-        $(this).toggleClass("clicked");
-        if($(this).hasClass("clicked")){$(".form_block").show();}
-        else if($(this).hasClass("trigger")){$(".form_block").hide();}
-    });
-    /* Inserting Values Into "Add New Category" Form When User Click "Edit" Category & "Add" Category*/
-    $(".cont .edit a, .add a").click(function(){
-        $(".trigger").addClass("clicked");
-        $("#addoperation_but").addClass("act");
-        $(".addoperation").show();
-        if($(".cont .edit a, .add a").hasClass("cat")){$(".addoperation").hide(); $("#addoperation_but").removeClass("act");}
-        var f_namecat = $(this).parent().parent().parent().parent().parent().find("td.w1 a").text(); // Get Category Name
-        $("#f_namecat").val(f_namecat); // Change "Add New Category" Form's Field
-        var f_catsys = $(this).parent().parent().parent().parent().parent().find("td.w3").text(); // Get System Category Name
-        // Function Searching System Category Name in Categories SelectBox And Make It Selected
-        var f_catsys_lenght = $("#f_catsys option").length;
-        for(var i=0; i<f_catsys_lenght; i++){
-            if($("#f_catsys option").eq(i).text() == f_catsys){$("#f_catsys option").eq(i).attr("selected","selected");}
-        }
-        var f_cattype = $(this).parent().parent().parent().parent().parent().find("td.w2").text(); // Get System Category Type Name
-        // Function Searching System Category Type Name in Category Types SelectBox And Make It Selected
-        var f_cattype_length = $("#f_cattype option").length;
-        for(var j=0; j<f_cattype_length; j++){
-            if($("#f_cattype option").eq(j).text() == f_cattype){$("#f_cattype option").eq(j).attr("selected","selected");}
-        }
-
-
-        var money_type = $(this).parent().parent().parent().parent().parent().find("td.el_6").text();
-        var f_oper_type = $(this).parent().parent().parent().parent().parent().find("td.el_2 a").text();
-        var amount = $(this).parent().parent().parent().parent().parent().find("td.el_3 b").text();
-        var category = $(this).parent().parent().parent().parent().parent().find("td.el_5").text();
-        var ftags = $(this).parent().parent().parent().parent().parent().find("td.el_7").text();
-        var fmes = $(this).parent().parent().parent().parent().parent().find("td.el_8 .cont p").text();
-        var fdate = $(this).parent().parent().parent().parent().parent().find("td.el_4").text();
-
-        var money_type_length = $("#money_type select option").length;
-        var f_oper_type_length = $("#f_oper_type select option").length;
-        var amount_length = $(".amount select option").length;
-        var category_length = $(".category select option").length;
-
-        $("#ftags").val(ftags);
-        $("#fmes").val(fmes);
-        $("#fdate").val(fdate);
-
-        // Searching Operation Type Name in Operation Types SelectBox And Make It Selected
-        for(j=0; j<f_oper_type_length; j++){
-            if($("#f_oper_type select option").eq(j).text() == f_oper_type){$("#f_oper_type select option").eq(j).attr("selected","selected");}
-        }
-
-        // Searching Amount Type Name in Operation Types SelectBox And Make It Selected
-        for(j=0; j<amount_length; j++){
-            if($(".amount select option").eq(j).text() == amount){$(".amount select option").eq(j).attr("selected","selected");}
-        }
-
-        // Searching Category Type Name in Category Types SelectBox And Make It Selected
-        for(j=0; j<category_length; j++){
-            if($(".category select option").eq(j).text() == category){$(".category select option").eq(j).attr("selected","selected");}
-        }
-
-        // Searching Money Type Name in Category Types SelectBox And Make It Selected
-        for(j=0; j<money_type_length; j++){
-            if($("#money_type select option").eq(j).text() == money_type){$("#money_type select option").eq(j).attr("selected","selected");}
-        }
-        
-    });
     /* Hiding Category Line */
     $(".cont .del a").click(function(){
         $(this).parent().parent().parent().parent().parent().hide();
     });
 
+    /* Show & Hide Popup Tags Edit Form */
+    $(".tags_list ul li").click(function(e){
+        $(this).children().toggleClass("edit"); // Creating Clicked Tag Marker for Identification by "Change" Button.
+        var tag_name = $(this).children().text(); // Getting Tag Name
+        var y = e.pageY; // Getting Cursor Vertical Position
+        $("#poputags_edit").css({'left' : 20 + 'px', 'top' : y - 10 + 'px', 'display' : 'block'}); // Show Popup Form
+        $("#poputags_edit #fptags").val(tag_name); // Inserting Into Popup Form Clicked Tag Name
+    });
+    /* Changing Tag Name */
+    $("#poputags_edit .but").click(function(){
+        var new_tag_name =  $("#poputags_edit #fptags").val(); // Getting New Tag Value From Popup Form
+        $(".tags_list ul li a.edit").text(new_tag_name).toggleClass("edit"); // Insering New Tag Name Into Link With Class "Edit" & Removing It's Marker
+        $("#poputags_edit").hide(); // Close Popup Window
+    });
+    /* Add New Tag */
+    $(".tags_list .add").click(function(e){
+        var y = e.pageY; // Getting Cursor Vertical Position
+        $("#poputags_add").css({'left' : 20 + 'px', 'top' : y - 10 + 'px', 'display' : 'block'}); // Show Popup Form
+    });
+    $("#poputags_add .but").click(function(){
+        var add_tag_name =  $("#poputags_add #fpatags").val(); // Getting New Tag Value From Popup Form
+        $(".tags_list ul").append('<li><a href="#">'+ add_tag_name +'</a></li>');
+        $("#poputags_add").hide(); // Close Popup Window
+    });
+
+    /* Join */
+    $(".join").click(function(){
+        $("#popupaddobject").show();
+        var target_name = $(this).parent().find("a.name").text();
+        $("#popupaddobject #fpname2").val(target_name);
+    });
+
+    /* Edit Target */
+    $(".f_f_edit, .f_f_copy").click(function(){
+        var object_name = $(".object .descr a").text();
+        $("#popupaddobject #fpname2").val(object_name);
+        $("#popupaddobject").show();
+    });
+    /* Remove Target */
+    $(".f_f_del").click(function(){
+        $(this).parent().parent().parent().hide();
+    });
+
+    /* Show Hided Objects */
+    $(".show_all span").click(function(){
+        $(this).slideUp("fast");
+        $(".hided").slideDown("fast");
+    });
 });
