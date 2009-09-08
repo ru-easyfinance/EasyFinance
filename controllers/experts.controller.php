d1808678a635c3b617f9b5fbad13803eb3924581
rewle <rewle@240b7755-b59a-4df0-b20f-7dab6991996d>
2009-09-08 07:12:33 +0000

    экстремальный коммит.
    git-svn-id: svn://hm.podzone.net/branches/old@409 240b7755-b59a-4df0-b20f-7dab6991996d

---
+++ controllers/experts.controller.php
@@ -0,0 +1,112 @@
+<?php if (!defined('INDEX')) trigger_error("Index required!",E_USER_WARNING);
+/**
+ * Класс контроллера для модуля экспертов
+ * @copyright http://home-money.ru/
+ * @version SVN $Id: accounts.controller.php 232 2009-08-21 14:34:45Z rewle $
+ */
+
+class Experts_Controller extends Template_Controller
+{
+
+    /**
+     * Ссылка на класс Smarty
+     * @var <Smarty>
+     */
+    private $tpl = null;
+
+    /**
+     * Ссылка на класс модель
+     * @var <Accounts_Model>
+     */
+    private $model = null;
+
+    /**
+     * Конструктор класса
+     * @return void
+     */
+    function __construct()
+    {
+        $this->tpl   = Core::getInstance()->tpl;
+        $this->model = new Experts_Model();
+    }
+
+    /**
+     * Индексная страница
+     * @return void
+     */
+    function index()
+    {
+        $this->model->index();
+
+        $this->tpl->assign('name_page', 'experts/experts');
+
+        $this->tpl->assign('desktop',($this->model->get_desktop()));//main div class
+        $js_list = $this->model->js_list();//array js with indeficators;
+
+        foreach ($js_list as $key=>$val)
+        {
+            $this->tpl->append('js',"experts/$val.js");
+        }
+
+    }
+//////////////////////////////////experts///////////////////////////////////////
+
+    function get_desktop_fields()//only experts//todo after dl
+    {
+        return false;
+    }
+
+    /** обновляет значения для панельки эксперта
+     *
+     */
+    function get_desktop_field()
+    {
+        $field_id = $_POST['field_id'];
+        if (!$field_id)
+            die();
+        die(json_encode($this->model->get_desktop_field($field_id)));
+    }
+
+    /** редактирование профиля эксперта
+     *
+     */
+    function update_expert()
+    {
+        $param['mini_desc'] = $_POST['mini_desc'];//str
+        $param['description'] = $_POST['description'];//str
+        $param['services'] = $_POST['services'];////array
+        $param['themes'] = $_POST['themes'];//array
+        $param['sertificat'] = $_POST['sertificat'];//str
+        die($this->model->update_expert());
+    }
+
+/////////////////////////////////users//////////////////////////////////////////
+
+    /*
+     * список всех экспертов с возможностью сортировки
+     * @todo возможно стоит хранить этот список в куки?будующее
+     */
+    function get_experts_list()//expert light info//only user
+    {
+        $order = $_POST['order'];
+        if (!$order)
+            $order = 'id';
+        die(json_encode($this->model->get_experts_list($order)));
+    }
+
+    /*
+     * полная инфа о эксперте.возможна сортировка.
+     */
+    function get_expert()//expert foolinfo
+    {
+        $expert_id = $_POST['expert_id'];
+        if (!$expert_id)
+            die();
+        die(json_encode($this->model->get_expert($expert_id)));
+    }
+
+    function add()//reit question etc//@todo after mail/
+    {
+
+    }
+}
