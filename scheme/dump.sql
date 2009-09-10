83d22f79605d4f3594d75607dbc1d22a463d0bc7
ukko <ukko@240b7755-b59a-4df0-b20f-7dab6991996d>
2009-09-10 14:26:17 +0000

    Обновил дамп для работы с валютами
    git-svn-id: svn://hm.podzone.net/branches/old@449 240b7755-b59a-4df0-b20f-7dab6991996d

--- scheme/dump.sql
+++ scheme/dump.sql
@@ -92,21 +92,27 @@ CREATE TABLE `category` (
 
 DROP TABLE  IF EXISTS`currency`;
 CREATE TABLE `currency` (
-  `cur_id` int(11) NOT NULL AUTO_INCREMENT,
-  `cur_name` varchar(20) NOT NULL DEFAULT '',
-  `cur_char_code` varchar(15) NOT NULL,
-  `cur_name_value` varchar(255) NOT NULL,
-  PRIMARY KEY (`cur_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+      `cur_id` int(11) NOT NULL AUTO_INCREMENT,
+      `cur_name` varchar(10) NOT NULL,
+      `cur_char_code` varchar(15) NOT NULL,
+      `cur_name_value` varchar(255) NOT NULL,
+      `cur_okv_id` varchar(4) NOT NULL,
+      `cur_country` varchar(255) NOT NULL COMMENT 'ИД страны',
+      `cur_uses` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT 'Обновлять курс',
+      PRIMARY KEY (`cur_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 DROP TABLE  IF EXISTS`daily_currency`;
 CREATE TABLE `daily_currency` (
-  `currency_id` int(11) NOT NULL,
-  `currency_date` date NOT NULL,
-  `currency_sum` varchar(11) NOT NULL,
-  `direction` enum('+','-','0') NOT NULL COMMENT 'Направление роста валюты. + = растёт, - = падает, 0 = без изменений',
-  KEY `new_index` (`currency_date`)
+      `currency_id` int(11) NOT NULL,
+      `currency_date` date NOT NULL,
+      `currency_sum` decimal(20,4) unsigned NOT NULL DEFAULT '0.0000' COMMENT 'Системный курс валют',
+      `direction` enum('+','-','0') NOT NULL COMMENT 'Направление роста валюты. + = растёт, - = падает, 0 = без изменений',
+      `currency_user_sum` decimal(20,4) unsigned NOT NULL DEFAULT '0.0000' COMMENT 'Пользовательский курс валют',
+      `user_id` bigint(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Ид пользователя',
+      KEY `new_index` (`currency_date`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 DROP TABLE  IF EXISTS`feedback_message`;
 CREATE TABLE `feedback_message` (
