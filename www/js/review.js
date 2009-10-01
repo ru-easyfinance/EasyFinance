b6381af7995957f5b1002e8bb70c57e10cadc2b4
challenger <challenger@240b7755-b59a-4df0-b20f-7dab6991996d>
2009-10-01 15:11:01 +0000

    добавил картинки и review.js
    git-svn-id: svn://hm.podzone.net/trunk@681 240b7755-b59a-4df0-b20f-7dab6991996d

---
+++ www/js/review.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+
+
+        $('body').mousemove(function(){
+            if (!$('.map div:hover').length)
+            {
+                $('.w_dialog.mapContent').hide();
+                for (var N=2; N<=10; N++)
+                $('.w_dialog.mapContent'+N).hide();
+            }
+           // return false;
+        })  
+        //$('#w_dialog.mapContent').css('position','absolute');
+        
+        //$('#w_dialog.mapContent').hide();
+
+	$('.map div').mouseover(function(){//.popup_map div
+		var c = $(this).attr('class');
+                //alert(c)
+		switch (c)
+		{
+			case 'registration':
+                            $('.w_dialog.mapContent').show();
+				break;
+			case 'CreateSales':
+                            $('.w_dialog.mapContent2').css('display','block');
+				break;
+			case 'categoryEdit':
+                            $('.w_dialog.mapContent3').css('display','block');
+				break;
+                        case 'closeOperation':
+                            $('.w_dialog.mapContent4').css('display','block');
+				break;
+                        case 'infoPanel':
+                            $('.w_dialog.mapContent5').css('display','block');
+				break;
+                         case 'createTransaction':
+                            $('.w_dialog.mapContent6').css('display','block');
+				break;
+                         case 'expertView':
+                            $('.w_dialog.mapContent7').css('display','block');
+				break;
+                         case 'analise':
+                            $('.w_dialog.mapContent8').css('display','block');
+				break;
+                         case 'financeTarget':
+                            $('.w_dialog.mapContent9').css('display','block');
+				break;
+                         case 'capitalCreate':
+                            $('.w_dialog.mapContent10').css('display','block');
+				break;
+		}
+                return false;
+	});
+        //$('#w_dialog.mapContent').hide();
+        $('.link.close').click(function(){
+            $('.w_dialog.mapContent').hide();
+            for (var N=1; N<=10; N++)
+            $('.w_dialog.mapContent'+N).hide();
+        });
+        <a href='http://www.microsoft.com/rus/windows/internet-explorer/'>http://www.microsoft.com/rus/windows/internet-explorer/</a>
+//У Вас установлена старая старая версия браузера!<br>Для стабильной работы Системы личного управления финансами<br> рекомендуем Вам обновить свой браузер
+});
+
+
