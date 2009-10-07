9fd6ef1e9e4b308545f02626c5c03358c5051525
rewle <rewle@240b7755-b59a-4df0-b20f-7dab6991996d>
2009-10-07 06:45:32 +0000

    бюджет.заморозка до вечера.
    git-svn-id: svn://hm.podzone.net/trunk@731 240b7755-b59a-4df0-b20f-7dab6991996d

--- www/js/budget.js
+++ www/js/budget.js
@@ -1,54 +1,4 @@
-//$(document).ready(function() {
-//
-//    $('#prev_month').click(function(){
-//        loadBudget('prev');
-//    });
-//
-//    $('#next_month').click(function(){
-//        loadBudget('next');
-//    });
-//
-//    $('#create_plan').click(function(){
-//        createPlan();
-//    });
-//
-//    // Autoload
-//    loadBudget('current');
-//
-//    function loadBudget(month)
-//    {
-//        $.ajax({
-//            type: "GET",
-//            url: "/budget/loadBudget/",
-//            data: {
-//                month: month,
-//                current_date: $('#current_date').val()
-//            },
-//            success: function(data) {
-//                $('#list').html(data);
-//            }
-//        });
-//    }
-//
-//    function createPlan()
-//    {
-//        $('#list').hide();
-//
-//        $.ajax({
-//            type: "GET",
-//            url: "/budget/create/",
-//            data: {
-//                current_date: $('#current_date').val()
-//            },
-//            success: function(data) {
-//                $('#form_create_plan').html(data);
-//            }
-//        });
-//    }
-//});
 $(document).ready(function() {
-
-
     /**
      * @desc объект отвечающий за работу бюджета
      */
@@ -156,5 +106,6 @@ budget.setup_list(
     }
 })
 ///////////////////////////////////
+//$('input#year').mask('9999');
 
 })
