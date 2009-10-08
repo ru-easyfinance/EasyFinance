3122f0f1889fee00749a3f6efc6a1b4183b8fa6d
ukko <ukko@240b7755-b59a-4df0-b20f-7dab6991996d>
2009-10-08 11:32:12 +0000

    Поправил файл схемы базы данных
    git-svn-id: svn://hm.podzone.net/trunk@743 240b7755-b59a-4df0-b20f-7dab6991996d

--- scheme/versions/24.sql
+++ scheme/versions/24.sql
@@ -1,1 +1,3 @@
-ALTER TABLE `calendar` ADD COLUMN `close` TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Статус цели, по умолчанию = 0, т.е. открыта' AFTER `category`;
+ALTER TABLE `calendar`
+ADD COLUMN `category` BIGINT(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Ид категории для периодических транзакций' AFTER `amount`,
+ADD COLUMN `close` TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Статус цели, по умолчанию = 0, т.е. открыта' AFTER `category`;
