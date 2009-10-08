4343840190630788d21b0f929553c4e8ff5e5db0
jet <jet@240b7755-b59a-4df0-b20f-7dab6991996d>
2009-10-08 13:01:22 +0000

    Внедряем новую объектную модель. Модуль, отвечающий за инициализацию и запуск главного объекта приложения.
    git-svn-id: svn://hm.podzone.net/trunk@748 240b7755-b59a-4df0-b20f-7dab6991996d

---
+++ www/js/easyfinance.js
@@ -0,0 +1,11 @@
+/*
+ * @module EasyFinance application
+ * @author Andrey [Jet] Zharikov
+ */
+var easyFinance = {};
+
+easyFinance.models = {};
+
+$(document).ready(function() {
+    easyFinance.models.category.load();
+});
