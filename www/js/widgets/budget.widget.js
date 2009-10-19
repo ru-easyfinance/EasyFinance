7356f462ed1ff51bd67c8655a12b21f3b45a0d16
rewle <rewle@240b7755-b59a-4df0-b20f-7dab6991996d>
2009-10-19 09:49:14 +0000

    поправил формирование jsona
    git-svn-id: svn://hm.podzone.net/trunk@866 240b7755-b59a-4df0-b20f-7dab6991996d

--- www/js/widgets/budget.widget.js
+++ www/js/widgets/budget.widget.js
@@ -104,6 +104,7 @@ $('#master input').live('keyup',function(e){
     $('.next').click(function(){
         date='01.'+$('#sel_date select').val()+'.'+$('#sel_date input').val();
         $('#master').dialog('open');
+        //$('.ui-dialog-titlebar #ui-dialog-title-master').text('Планирование бюджета на '+$('#sel_date select option').val())
         $('#master #b_save').hide()
         $('#master .button').click(function(){
             $(this).hide();
@@ -172,7 +173,7 @@ $('#master input').live('keyup',function(e){
                 }
             })
             ret[id] +=']';//@deprecate переписать
-        var r_str = '{"1":'+ret[1]+'},"0":{'+ret[0]+'}';
+        var r_str = '{"1":'+ret[1]+'},{"0":'+ret[0]+'}';
         $.post('/budget/add/',{data:r_str.replace(/,]/gi, ']'),start:date} , function(data){
             model.load(data);
             $('#master .button').show()
