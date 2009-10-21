a8934e510010740972ec183c7fa72e764f2e4dda
challenger <challenger@240b7755-b59a-4df0-b20f-7dab6991996d>
2009-10-21 13:58:19 +0000

    при входе в демо первый раз выскакивает гид
    git-svn-id: svn://hm.podzone.net/trunk@935 240b7755-b59a-4df0-b20f-7dab6991996d

--- models/login.model.php
+++ models/login.model.php
@@ -256,6 +256,7 @@ class Login_Model
 		if ( $user->initUser($auth['login'], $auth['pass'] ) )
 		{
 			setcookie(COOKIE_NAME, encrypt(array($auth['login'], $auth['pass'])), 0, COOKIE_PATH, COOKIE_DOMEN, COOKIE_HTTPS);
+                        setCookie("guide", "uyjsdhf",0,COOKIE_PATH, COOKIE_DOMEN, false);
 			session_commit();
 			header("Location: /info");
 			exit;
