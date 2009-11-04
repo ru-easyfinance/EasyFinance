ce6ebacc8c0e1f1c2ee8407670d6b7800590b267
ukko <ukko@240b7755-b59a-4df0-b20f-7dab6991996d>
2009-11-04 09:50:07 +0000

    *
    git-svn-id: svn://hm.podzone.net/trunk@1111 240b7755-b59a-4df0-b20f-7dab6991996d

--- classes/Account/Model.php
+++ classes/Account/Model.php
@@ -1,10 +1,23 @@
-<?php
-//echo ('12');
+<?php if (!defined('INDEX')) trigger_error("Index required!",E_USER_WARNING);
+/**
+ * 
+ */
 class Account_Model {
 
+    /**
+     *
+     * @var <type>
+     */
     private $db = null;
     //private $user = null;
 
+    /**
+     *
+     * @param <type> $acc
+     * @param <type> $rec
+     * @param <type> $ch
+     * @param <type> $del
+     */
     function __construct($acc, $rec, $ch, $del){
         //echo ('12');
         //$this->db=DbSimple_Generic::connect("mysql://".SYS_DB_USER.":".SYS_DB_PASS."@".SYS_DB_HOST."/".SYS_DB_BASE);
@@ -15,7 +28,18 @@ class Account_Model {
             $this->addAccount($v['remotekey'],$v['name'],$v['cur'],$v['date'],$v['startbalance'],$v['descr']);
         }
         
-    }//*/
+    }
+
+    /**
+     *
+     * @param <type> $id
+     * @param <type> $name
+     * @param <type> $curid
+     * @param <type> $date
+     * @param <type> $amount
+     * @param <type> $descr
+     * @param <type> $user_id
+     */
     function addAccount($id=0, $name='', $curid=0, $date='', $amount=0, $descr='', $user_id){
         echo ('123');
         /* $cur=1;
@@ -24,14 +48,31 @@ class Account_Model {
             `account_currency_id`, `user_id`) VALUES (?,?,?,?,?);";
         return $this->db->query($sql, $name, 1, $descr, $cur, $user_id);*/
     }
+
+    /**
+     *
+     * @param <type> $id
+     * @param <type> $name
+     * @param <type> $curid
+     * @param <type> $date
+     * @param <type> $amount
+     * @param <type> $descr
+     * @param <type> $user_id
+     * @return <type>
+     */
     function editAccount($id=0, $name='', $curid=0, $date='', $amount=0, $descr='', $user_id){
         $sql = "UPDATE accounts SET (`account_name`, `account_type_id`, `account_description`,
             `account_currency_id`, `user_id`) VALUES (?,?,?,?,?) WHERE account_id =?;";
         return $this->db->query($sql, $name, 1, $descr, $cur, $user_id, $id);
     }
+
+    /**
+     *
+     * @param <type> $id
+     * @return <type>
+     */
     function deleteAccount($id=0){
         $sql="DELETE FROM accounts WHERE account_id =?;";
         return $this->db->query($sql, $id);
     }
 }
-?>
