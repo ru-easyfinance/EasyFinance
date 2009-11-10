d092c46cdd661e0a41304aee6bcf0a4f4ccbe0f1
jet <jet@240b7755-b59a-4df0-b20f-7dab6991996d>
2009-11-10 10:42:47 +0000

    * меню бюджет
    git-svn-id: svn://hm.podzone.net/trunk@1185 240b7755-b59a-4df0-b20f-7dab6991996d

--- www/js/main.js
+++ www/js/main.js
@@ -647,9 +647,9 @@ $('.tags_list .add').live('click', function(){
                         </ul> \
                 </li> \
                 <li id="m3"> \
-                        <a href="#"></a> \
+                        <a href="/budget/"></a> \
                         <ul> \
-                                <!-- <li><span/><a href="/budget/">Бюджет</a></li> --> \
+                                <li><span/><a href="/budget/">Бюджет</a></li> \
                                 <li><span/><a href="/targets/">Фин. цели</a></li> \
                                 <li class="last"><img src="/img/i/menu3_submenu_bottom.png"/></li> \
                         </ul> \
