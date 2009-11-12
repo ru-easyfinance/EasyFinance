0fec2b5ed471cb4e7f22831f3d88ee662fcc88a7
Rewle <Rewle@240b7755-b59a-4df0-b20f-7dab6991996d>
2009-11-12 13:57:59 +0000

    бюджет.новая версия.для отладки.правки
    git-svn-id: svn://hm.podzone.net/trunk@1221 240b7755-b59a-4df0-b20f-7dab6991996d

--- www/js/budget.js
+++ www/js/budget.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
-
-var budget = easyFinance.models.budget();
-budget.load(res.budget);
-easyFinance.widgets.budget(budget);
-//easyFinance.widgets.budgetMaster(budget, widget);
+    var budget = easyFinance.models.budget();
+    budget.load(res.budget);
+    easyFinance.models.category.load(function(){
+        var widget = easyFinance.widgets.budget(budget);
+        easyFinance.widgets.budgetMaster(budget, widget);
+    });
 })
