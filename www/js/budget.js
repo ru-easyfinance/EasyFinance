39706807e0c1e04fdb3554054f712f113ebfaf53
jet <jet@240b7755-b59a-4df0-b20f-7dab6991996d>
2009-11-12 09:31:15 +0000

    *
    git-svn-id: svn://hm.podzone.net/trunk@1210 240b7755-b59a-4df0-b20f-7dab6991996d

--- www/js/budget.js
+++ www/js/budget.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
 
 var budget = easyFinance.models.budget();
 budget.load(res.budget);
-var widget = easyFinance.widgets.budget(budget);
-easyFinance.widgets.budgetMaster(budget, widget);
+easyFinance.widgets.budget(budget);
+//easyFinance.widgets.budgetMaster(budget, widget);
 })
