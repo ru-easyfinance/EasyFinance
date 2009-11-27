8ecab9ed5149340c5fc00b65a72983fb28496d91
challenger <challenger@240b7755-b59a-4df0-b20f-7dab6991996d>
2009-11-27 12:55:58 +0000

    поправил подключение к серверу
    git-svn-id: svn://hm.podzone.net/trunk@1476 240b7755-b59a-4df0-b20f-7dab6991996d

--- www/seo.php
+++ www/seo.php
@@ -1,4 +1,101 @@
+<html>
+<table width="100%" border="1">
+<tr>
+    <th width="50%">
 <?php
+
+class SeoText{
+    private $name;
+    private $text1;
+    private $text2;
+    private $array = array();
+
+    function SeoText(){
+        $this->name = $_POST['name'];
+        $this->text1 = $_POST['maintext'];
+        $this->text2 = $_POST['relatedtext'];
+    }
+    
+    function GetArray() {
+        $f = fopen('admin/seo.txt','r');
+        if (filesize('admin/seo.txt')){
+            $content = fread($f,filesize('admin/seo.txt'));
+            fclose($f);
+            $ArrString = explode('\n',$content);
+            foreach ($ArrString as $key=>$v){
+                $str = explode('\t',$v);
+                $this->array[$key][0] = $str[0];
+                $this->array[$key][1] = $str[1];
+                $this->array[$key][2] = $str[2];             
+            };
+        }
+    }
+    
+    function ShowAll(){
+        foreach ($this->array as $k=>$v){
+            if ($v[0]!=''){
+                $button = '<form name="del" method="post" action="/seo.php"><input name="delname" type="hidden" value="'.$v[0].'"><input type="submit" value="Удалить"></form>';
+                $button .= '<form name="edit" method="post" action="/seo.php"><input name="editname" type="hidden" value="'.$v[0].'"><input type="submit" value="Редактировать"></form>';
+                echo($v[0].'     '.$button);
+                echo('<xmp>'.$v[1].'</xmp>');
+                echo('<xmp>'.$v[2].'</xmp>'.'<br>');
+            }
+        }
+    }
+
+    function PlusToFile() {
+        $arr = array($this->name, $this->text1, $this->text2);
+        //print_r($this->array);
+        $this->array[] = $arr;
+        $dump = $this->name.'\t'.$this->text1.'\t'.$this->text2.'\n';
+        $f = fopen('admin/seo.txt', 'a');
+        fwrite($f, $dump);
+        fclose($f);
+    }
+
+    function DeleteRecord($name){
+        $f = fopen('admin/seo.txt','r');
+        if (filesize('admin/seo.txt')){
+            $content = fread($f,filesize('admin/seo.txt'));
+            fclose($f);
+            $ArrString = explode('\n',$content);
+            $f = fopen('admin/seo.txt', 'w');
+            $dump='';
+            foreach ($ArrString as $key=>$v){
+                $str = explode('\t',$v);
+                if ($str[0]!=$name) {
+                    $dump .= $str[0].'\t'.$str[1].'\t'.$str[2].'\n';
+                }
+            };
+            fwrite($f, $dump);
+            fclose($f);
+        } 
+    }
+
+    function EditString($name, $text1, $text2){
+        $f = fopen('admin/seo.txt','r');
+        if (filesize('admin/seo.txt')){
+            $content = fread($f,filesize('admin/seo.txt'));
+            fclose($f);
+            $ArrString = explode('\n',$content);
+            $f = fopen('admin/seo.txt', 'w');
+            $dump='';
+            foreach ($ArrString as $key=>$v){
+                $str = explode('\t',$v);
+                if ($str[0]!=$name) {
+                    $dump .= $str[0].'\t'.$str[1].'\t'.$str[2].'\n';
+                }else{
+                    $dump .= $name.'\t'.$text1.'\t'.$text2.'\n';
+                }
+
+            };
+            fwrite($f, $dump);
+            fclose($f);
+        }
+    }
+}
+
+
 if (!(isset($_SERVER['PHP_AUTH_USER']) &&
     isset($_SERVER['PHP_AUTH_PW']) &&
     $_SERVER['PHP_AUTH_USER'] == 'megaseo' &&
@@ -7,53 +104,72 @@ if (!(isset($_SERVER['PHP_AUTH_USER']) &&
     area"');
   header('Status: 401 Unauthorized');
 } else {
-if (isset($_POST['name'])){
-    echo ($_POST['name']);
+
+    if (isset($_POST['edname']) && isset($_POST['edmaintext']) && isset($_POST['edrelatedtext'])){
+        $seo = new SeoText();
+        $seo->GetArray();
+        $seo->EditString($_POST['edname'],$_POST['edmaintext'],$_POST['edrelatedtext']);
+        $seo->ShowAll();
+    }
+
+if (isset($_POST['delname'])){
+    $seo = new SeoText();
+    $seo->DeleteRecord($_POST['delname']);
+    $seo->GetArray();
+    $seo->ShowAll();
+}else{
+
+        if (isset($_POST['name']) && isset($_POST['maintext']) && isset($_POST['relatedtext'])){
+            $seo = new SeoText();
+            $seo->GetArray();
+            $seo->PlusToFile();
+            $seo->ShowAll();
+        }else{
+            $seo = new SeoText();
+            $seo->GetArray();
+            $seo->ShowAll();
+        }
+
+
+
 }
+    if (!isset($_POST['editname'])){
 ?>
-<script type="text/javascript" src="admin/tinymce/jscripts/tiny_mce/tiny_mce.js"></script>
-<script type="text/javascript">
-tinyMCE.init({
-
-// General options
-
-mode : "textareas",
-
-theme : "advanced",
-
-plugins : "safari,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,imagemanager,filemanager",
-
-theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect,|,bullist,numlist,|,undo,redo",
-
-//theme_advanced_buttons2 : "bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
-
-//theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
-
-//theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,blockquote,pagebreak,|,insertfile,insertimage",
-theme_advanced_toolbar_location : "top",
-theme_advanced_toolbar_align : "left",
-theme_advanced_statusbar_location : "bottom",
-//theme_advanced_resizing : true,
-content_css : "css/example.css",
-template_external_list_url : "js/template_list.js",
-external_link_list_url : "js/link_list.js",
-//external_image_list_url : "js/image_list.js",
-//media_external_list_url : "js/media_list.js"
-});
-</script>
-<html>
+        </th>
+        <td>
     <form name="test" method="post" action="/seo.php">
     <p>Заголовок<Br>
     <textarea name="name" cols="40" rows="3"></textarea></p>
     <p>Основной текст<Br>
-    <textarea name="mailtext" cols="40" rows="6"></textarea></p>
+    <textarea name="maintext" cols="40" rows="6"></textarea></p>
     <p>Расширенный текст<Br>
     <textarea name="relatedtext" cols="40" rows="6"></textarea></p>
 
-    <p><input type="submit" value="Отправить">
-    <input type="reset" value="Очистить"></p>
+    <p><input name="add"type="submit" value="Добавить">
     </form>
-</html>
+
 <?php
+    }
+    else{
+        ?>
+            </th>
+        <td>
+    <form name="test" method="post" action="/seo.php">
+    <p>Заголовок<Br>
+    <textarea name="edname" cols="40" rows="3"><?$_POST['editname']?></textarea></p>
+    <p>Основной текст<Br>
+    <textarea name="edmaintext" cols="40" rows="6"></textarea></p>
+    <p>Расширенный текст<Br>
+    <textarea name="edrelatedtext" cols="40" rows="6"></textarea></p>
+
+    <p><input name="add"type="submit" value="Редактировать">
+    </form>
+
+        <?
+    }
 }
 ?>
+        </td>
+</tr>
+</table>
+</html>
