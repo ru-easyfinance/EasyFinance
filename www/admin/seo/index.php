d3d1568b57c0cb6c043ef501a1ba253fb9a5ae0d
challenger <challenger@240b7755-b59a-4df0-b20f-7dab6991996d>
2009-11-30 14:14:19 +0000

    seo qwerty
    git-svn-id: svn://hm.podzone.net/trunk@1525 240b7755-b59a-4df0-b20f-7dab6991996d

--- www/admin/seo/index.php
+++ www/admin/seo/index.php
@@ -1,55 +1,119 @@
 <?php
+
+/**
+ * Логин сео-оптимизатора
+ */
+define ('SEO_LOGIN', 'seo');
+
+/**
+ * Пароль сео-оптимизатора
+ */
+define ('SEO_PASS', 'qwerty');
+
+/**
+ * Путь до файла с массивом сео
+ */
+define ('SEO_FILENAME', '../seo.php');
+
 class SeoText{
+
+    /**
+     * Заголовок редактируемой / добавляемой записи
+     * @var string
+     */
     private $name;
+
+    /**
+     * Основной - видимый текст добавляемой / редактируемой записи
+     * @var string
+     */
     private $text1;
+
+    /**
+     * Расширенный текст добавляемой / редактируемой записи
+     * @var string
+     */
     private $text2;
+
+    /**
+     * Массив со считываемыми из файла данными
+     * @var array mixed
+     */
     private $array = array();
 
+
+
+    /**
+     * Заполняет переменные класса редактируемой / добавляемой записи
+     * @return void
+     */
     function SeoText(){
-        $this->name = addslashes($_POST['name']);
+        $this->name  = addslashes($_POST['name']);
         $this->text1 = addslashes($_POST['maintext']);
         $this->text2 = addslashes($_POST['relatedtext']);
     }
-    
+
+    /**
+     * Считывание данных из файла в массив
+     * @return void
+     */
     function GetArray() {
-        if (filesize('../seo.php')){
-            include '../seo.php';
+        if (file_exists(SEO_FILENAME)) {
+            include SEO_FILENAME;
             $this->array = $texts;
         }
     }
-    
-    function ShowAll(){
+
+    /**
+     * Вывод значений из массива списком для редактирования
+     * @return string
+     */
+    function ShowAll() {
+        $lists = '';
         foreach ($this->array as $k=>$v){
-            if ($v[0]!=''){
-                $button = '<form name="del" method="post" action="/admin/seo/"><input name="delname" type="hidden" value="'.$v[0].'"><input type="submit" value="Удалить"></form>';
-                $button .= '<form name="edit" method="post" action="/admin/seo/"><input name="editname" type="hidden" value="'.$v[0].'"><input name="edittext1" type="hidden" value="'.$v[1].'">
-                    <input name="edittext2" type="hidden" value="'.$v[2].'"><input type="submit" value="Редактировать"></form>';
-                echo('<table border="1" bordercolor="#333333">'.$v[0].'     '.$button);
-                echo('<br>'.$v[1].''.'<br>');
-                echo(''.$v[2].''.'<br></table>');
-            }
+            $button = '<form name="del" method="post" action="/admin/seo/">
+                <input name="delname" type="hidden" value="' . $v[0] . '" />
+                <input type="submit" value="Удалить" /></form>';
+            $button .= '<form name="edit" method="post" action="/admin/seo/">
+                <input name="editname" type="hidden" value="' . $v[0]  . '" />
+                <input name="edittext1" type="hidden" value="' . $v[1] . '" />
+                <input name="edittext2" type="hidden" value="' . $v[2] . '" />
+                <input type="submit" value="Редактировать" /></form>';
+
+            $lists .= '<table border="1" width="100%"><tr><th>' . $v[0] . $button . '</th></tr>';
+            $lists .=  '<tr><td>' . $v[1] . '</td></tr><tr><td>' . $v[2] . '</td></tr></table>';
         }
+        return $lists;
     }
 
+    /**
+     * Запись массива в файл
+     * @return void
+     */
     function AppendToFile() {
         $arr = array($this->name, $this->text1, $this->text2);
         $this->array[] = $arr;
-        $f = fopen('../seo.php', 'w');
-        $dump = '<?php $texts = ' . var_export( $this->array , true ) . ' ?>';//*/
+        $f = fopen(SEO_FILENAME, 'w');
+        $dump = '<?php $texts = ' . var_export( $this->array , true ) . ' ?>';
         fwrite($f, $dump);
         fclose($f);
     }
 
-    function DeleteRecord($name){
-        $f = fopen('../seo.php','r');
-        if (filesize('../seo.php')){
-            include '../seo.php';
+    /**
+     * Удаляет запись из массива
+     * @param string $name 
+     */
+    function DeleteRecord($name) {
+
+        if ( file_exists(SEO_FILENAME) ) {
+            include SEO_FILENAME;
             $this->array = $texts;
 
-            $f = fopen('../seo.php', 'w');
+            $f = fopen(SEO_FILENAME, 'w');
             foreach ($this->array as $k=>$value){
-                if ($this->array[$k][0] == $name)
-                    $this->array[$k] = '';
+                if ($this->array[$k][0] == $name) {
+                    unset ($this->array[$k]);
+                }
             }
             $dump = '<?php $texts = ' . var_export( $this->array , true ) . ' ?>';//*/
             fwrite($f, $dump);
@@ -57,133 +121,108 @@ class SeoText{
         } 
     }
 
+    /**
+     * Редактирует запись в массиве и записывает изменения в файл
+     * @param string $name
+     * @param string $text1
+     * @param string $text2
+     */
     function EditString($name, $text1, $text2){
-        $f = fopen('../seo.php','r');
-        if (filesize('../seo.php')){
-            include '../seo.php';
+        $f = fopen(SEO_FILENAME,'r');
+        if (file_exists(SEO_FILENAME)){
+            include SEO_FILENAME;
             $this->array = $texts;
 
-            $f = fopen('../seo.php', 'w');
+            $f = fopen(SEO_FILENAME, 'w');
             foreach ($this->array as $k=>$value){
                 if ($this->array[$k][0] == $name){
                     $arr = array($name, $text1, $text2);
                     $this->array[$k] = $arr;
                 }
             }
-            $dump = '<?php $texts = ' . var_export( $this->array , true ) . ' ?>';//*/
+            $dump = '<?php $texts = ' . var_export( $this->array , true ) . ' ?>';
             fwrite($f, $dump);
             fclose($f);
         }
     }
+    function AddEdit(){
+        $isnew = 0;
+        foreach ( $this->array as $k => $v ){
+            if ($v[0] == $_POST['name']) {
+                $isnew = $k;
+            }
+        }
+        if (!$isnew){
+            $this->AppendToFile();
+        }else{
+            $this->EditString($_POST['name'],$_POST['maintext'],$_POST['relatedtext']);
+        }
+    }
 }
 
-
+// Авторизация пользователя, если пользователь ввёл не верный логин или пароль!
 if (!(isset($_SERVER['PHP_AUTH_USER']) &&
-    isset($_SERVER['PHP_AUTH_PW']) &&
-    $_SERVER['PHP_AUTH_USER'] == 'seo' &&
-    $_SERVER['PHP_AUTH_PW'] == 'qwerty')) {
-  header('WWW-Authenticate: Basic realm="Secured
-    area"');
-  header('Status: 401 Unauthorized');
-} else {
-    ?>
-                <html>
-                    <head>
-                        <script type="text/javascript" src="/js/jquery/jquery-1.3.2.js"></script>
-                        <script type="text/javascript" src="/js/jquery/jHtmlArea-0.6.0.js"></script>
-                        <script type="text/css" src="/css/jquery/jHtmlArea.css"></script>
-                        <link rel="Stylesheet" type="text/css" href="/css/jquery/jHtmlArea.css" />
+    isset($_SERVER['PHP_AUTH_PW']) && 
+    $_SERVER['PHP_AUTH_USER'] == SEO_LOGIN && 
+    $_SERVER['PHP_AUTH_PW'] == SEO_PASS)) {
+    header('WWW-Authenticate: Basic realm="Secured area"');
+    header('Status: 401 Unauthorized');
+    exit;
+}
 
+// Редактирование
+/*if (isset($_POST['edname']) && isset($_POST['edmaintext']) && isset($_POST['edrelatedtext'])) {
+    $seo = new SeoText();
+    $seo->EditString($_POST['edname'],$_POST['edmaintext'],$_POST['edrelatedtext']);
+}*/
 
-                    </head>
-                    <body>
-<table width="100%" border="1">
-<tr>
-<td valign="top">
-    <?
-
-    if (isset($_POST['edname']) && isset($_POST['edmaintext']) && isset($_POST['edrelatedtext'])){
-        $seo = new SeoText();
-        $seo->EditString($_POST['edname'],$_POST['edmaintext'],$_POST['edrelatedtext']);
-        $seo->GetArray();
-        $seo->ShowAll();
-    }
-
-if (isset($_POST['delname'])){
+// Удаление
+if (isset($_POST['delname'])) {
     $seo = new SeoText();
     $seo->DeleteRecord($_POST['delname']);
+}
+
+// Добавление
+if (isset($_POST['name']) && isset($_POST['maintext']) && isset($_POST['relatedtext'])){
+    $seo = new SeoText();
     $seo->GetArray();
-    $seo->ShowAll();
-}else{
-
-        if (isset($_POST['name']) && isset($_POST['maintext']) && isset($_POST['relatedtext'])){
-            $seo = new SeoText();
-            $seo->GetArray();
-            $seo->AppendToFile();
-            $seo->ShowAll();
-        }else if (!isset($_POST['edname'])) if (!isset($_POST['delname'])){
-            $seo = new SeoText();
-            $seo->GetArray();
-            $seo->ShowAll();
+    $seo->AddEdit();
+    /*$isnew = 0;
+    foreach ( $this->array as $k => $v ){
+        if ($v[0] == $_POST['name']) {
+            $isnew = $k;
         }
-
-
-
-}
-    if (!isset($_POST['editname'])){
-?>
-        </td>
-        <td id="col2" width="25%">
-    <form name="test" method="post" action="/admin/seo/">
-    <p>Заголовок<Br>
-    <textarea name="name" cols="40" rows="3"></textarea></p>
-    <p>Основной текст<Br>
-    <textarea class="editor" name="maintext" cols="40" rows="6"></textarea></p>
-    <p>Расширенный текст<Br>
-    <textarea class="editor" name="relatedtext" cols="40" rows="6"></textarea></p>
-
-    <p><input name="add" type="submit" value="Добавить">
-    </form>
-<?php
     }
-    else{
-        ?>
-            </td>
-        <td id="col2" width="25%">
-    <form name="test" method="post" action="/admin/seo/">
-    <p>Заголовок<Br>
-    <textarea name="edname" cols="40" rows="3"><?echo($_POST['editname'])?></textarea></p>
-    <p>Основной текст<Br>
-    <textarea class="editor" name="edmaintext" cols="40" rows="6"><?echo($_POST['edittext1'])?></textarea></p>
-    <p>Расширенный текст<Br>
-    <textarea class="editor" name="edrelatedtext" cols="40" rows="6"><?echo($_POST['edittext2'])?></textarea></p>
-
-    <p><input name="edit" type="submit" value="Редактировать">
-    </form>
-
-        <?
-    }
+    if (!$isnew){
+        $seo->AppendToFile();
+    }else{
+        $seo->EditString($_POST['name'],$_POST['maintext'],$_POST['relatedtext']);
+    }*/
+// Вывод информации при первом запуске
+} else if (!isset($_POST['edname'])) if (!isset($_POST['delname'])) {
+    $seo = new SeoText();
 }
-?>
-        </td>
-</tr>
-</table>
-    <script type="text/javascript">
-        //alert('1');
-        $(".editor").htmlarea({
-            toolbar: [
-                    ["bold", "italic", "underline", "|", "forecolor"],
-                    ["h1", "h2", "h3", "h4", "h5", "h6"]
-                ],
 
-            css: "/css/jquery/jHtmlArea.css",
-            loaded: function() {
+$seo->GetArray();
+$lists = $seo->ShowAll();
 
-                    //// 'this' is equal to the jHtmlArea object
-                    //alert("jHtmlArea has loaded!");
-                    //this.showHTMLView(); // show the HTML view once the editor has finished loading
-                }
-        });
-    </script>
-</body>
-</html>
+$filename = dirname(__FILE__) . '/index.html';
+$handle   = fopen($filename, "r");
+$contents = fread($handle, filesize($filename));
+fclose($handle);
+
+if (isset($_POST['editname'])) {
+    $editname = $_POST['editname'];
+    $edittext1 = $_POST['edittext1'];
+    $edittext2 = $_POST['edittext2'];
+} else {
+    $editname = '';
+    $edittext1 = '';
+    $edittext2 = '';
+}
+$contents = str_replace('__LISTS__', $lists, $contents);
+$contents = str_replace('__NAME__', $editname, $contents);
+$contents = str_replace('__EDITOR__', $edittext1, $contents);
+$contents = str_replace('__RELATEDTEXT__', $edittext2, $contents);
+
+die($contents);
