f2bef9580d6a2cc6060ad56e60be19b1ded69737
challenger <challenger@240b7755-b59a-4df0-b20f-7dab6991996d>
2009-12-08 11:20:12 +0000

    отображение на клиенте старых операций из демо-аккаунта
    git-svn-id: svn://hm.podzone.net/trunk@1647 240b7755-b59a-4df0-b20f-7dab6991996d

--- www/js/widgets/operations/operationsJournal.widget.js
+++ www/js/widgets/operations/operationsJournal.widget.js
@@ -54,7 +54,7 @@ easyFinance.widgets.operationsJournal = function(){
             if (data[v].account_name == null)
                 continue;
 
-            if (data[v].tr_id != null && data[v].tr_id != '' && data[v].tr_id != '0' && data[v].transfer != "0") {
+            if (data[v].tr_id != null && data[v].tr_id != '' || ( data[v].tr_id == '0' && data[v].transfer != "0" ) ) {
                 tp = 'Перевод';
             } else if (data[v].virt == 1) {
                 tp = 'Фин.цель';
