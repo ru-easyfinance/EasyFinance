72a4ad0dc81eeba88e2f5f55c03f40320de633ce
jet <jet@240b7755-b59a-4df0-b20f-7dab6991996d>
2009-12-11 13:18:48 +0000

    * #519. Баннер книги для зарегистрированных пользователей
    git-svn-id: svn://hm.podzone.net/trunk@1698 240b7755-b59a-4df0-b20f-7dab6991996d

--- controllers/mail.controller.php
+++ controllers/mail.controller.php
@@ -5,7 +5,7 @@
  * @version SVN $Id: $
  */
 
-class Mail_Controller extends Template_Controller
+class Mail_Controller extends _Core_Controller_UserExpert
 {
     /**
      * Ссылка на класс модель
@@ -13,15 +13,12 @@ class Mail_Controller extends Template_Controller
      */
     private $model = null;
 
-
-    private $tpl = null;
     /**
      * Конструктор класса
      * @return void
      */
-    function __construct()
+    function __init()
     {
-        $this->tpl = Core::getInstance()->tpl;
         $this->model = new Mail_Model();
     }
 
@@ -43,9 +40,9 @@ class Mail_Controller extends Template_Controller
      */
      function inbox(){
         $jsonTest = '{
-            1: {folder: "inbox", unread: true, subject: "Входящее Письмо 1", from: "Эксперт", to: "Jet", date: "21.11.1986"},
-            2: {folder: "inbox", unread: false, subject: "Входящее Письмо 2", from: "Любофф", to: "Jet", date: "22.11.1986"},
-            3: {folder: "inbox", unread: false, subject: "Входящее Письмо 3", from: "Гуру", to: "Jet", date: "23.11.1986"},
+            1: {folder: "inbox", unread: true, subject: "Входящее Письмо 1", senderName: "Эксперт", receiverName: "Jet", senderId: 1, receiverId: 2, date: "21.11.1986"},
+            2: {folder: "inbox", unread: false, subject: "Входящее Письмо 2", senderName: "Любофф", receiverName: "Jet", senderId: 1, receiverId: 2, date: "22.11.1986"},
+            3: {folder: "inbox", unread: false, subject: "Входящее Письмо 3", senderName: "Гуру", receiverName: "Jet", senderId: 1, receiverId: 2, date: "23.11.1986"},
         }';
 
         die($jsonTest);
@@ -56,9 +53,9 @@ class Mail_Controller extends Template_Controller
      */
      function outbox(){
         $jsonTest = '{
-            4: {folder: "outbox", subject: "Исходящее Письмо 1", to: "Эксперт", from: "Jet", date: "21.11.1986"},
-            5: {folder: "outbox", subject: "Исходящее Письмо 2", to: "Любофф", from: "Jet", date: "22.11.1986"},
-            6: {folder: "outbox", subject: "Исходящее Письмо 3", to: "Гуру", from: "Jet", date: "23.11.1986"},
+            4: {folder: "outbox", subject: "Исходящее Письмо 1", receiverName: "Эксперт", senderName: "Jet", senderId: 1, receiverId: 2, date: "21.11.1986"},
+            5: {folder: "outbox", subject: "Исходящее Письмо 2", receiverName: "Любофф", senderName: "Jet", senderId: 1, receiverId: 2, date: "22.11.1986"},
+            6: {folder: "outbox", subject: "Исходящее Письмо 3", receiverName: "Гуру", senderName: "Jet", senderId: 1, receiverId: 2, date: "23.11.1986"},
         }';
 
         die($jsonTest);
@@ -69,9 +66,9 @@ class Mail_Controller extends Template_Controller
      */
      function drafts(){
         $jsonTest = '{
-            7: {folder: "drafts", subject: "Черновик 1", to: "Эксперт", from: "Jet", date: "21.11.1986"},
-            8: {folder: "drafts", subject: "Черновик 2", to: "Любофф", from: "Jet", date: "22.11.1986"},
-            9: {folder: "drafts", subject: "Черновик 3", to: "Гуру", from: "Jet", date: "23.11.1986"},
+            7: {folder: "drafts", subject: "Черновик 1", receiverName: "Эксперт", senderName: "Jet", senderId: 1, receiverId: 2, date: "21.11.1986"},
+            8: {folder: "drafts", subject: "Черновик 2", receiverName: "Любофф", senderName: "Jet", senderId: 1, receiverId: 2, date: "22.11.1986"},
+            9: {folder: "drafts", subject: "Черновик 3", receiverName: "Гуру", senderName: "Jet", senderId: 1, receiverId: 2, date: "23.11.1986"},
         }';
 
         die($jsonTest);
@@ -82,9 +79,9 @@ class Mail_Controller extends Template_Controller
      */
      function trash(){
         $jsonTest = '{
-            10: {folder: "trash", subject: "Удалённое Письмо 1", to: "Эксперт", from: "Jet", date: "21.11.1986"},
-            11: {folder: "trash", subject: "Удалённое Письмо 2", to: "Любофф", from: "Jet", date: "22.11.1986"},
-            12: {folder: "trash", subject: "Удалённое Письмо 3", to: "Гуру", from: "Jet", date: "23.11.1986"},
+            10: {folder: "trash", subject: "Удалённое Письмо 1", receiverName: "Эксперт", senderName: "Jet", senderId: 1, receiverId: 2, date: "21.11.1986"},
+            11: {folder: "trash", subject: "Удалённое Письмо 2", receiverName: "Любофф", senderName: "Jet", senderId: 1, receiverId: 2, date: "22.11.1986"},
+            12: {folder: "trash", subject: "Удалённое Письмо 3", receiverName: "Гуру", senderName: "Jet", senderId: 1, receiverId: 2, date: "23.11.1986"},
         }';
 
         die($jsonTest);
@@ -94,30 +91,30 @@ class Mail_Controller extends Template_Controller
      * отдаёт список писем во всех папка
      * @return json список писем
      */
-    function mail_list()
+    function listall()
     {
         //die(json_encode($this->model->mail_list()));
 
         $jsonTest = '{
             inbox: {
-                1: {folder: "inbox", unread: true, subject: "Входящее письмо с очень длинным заголовком", from: "Дядя Стёпа Милиционер", to: "Jet", date: "21.11.1986"},
-                2: {folder: "inbox", unread: false, subject: "Входящее Письмо 2", from: "Любофф", to: "Jet", date: "22.11.1986"},
-                3: {folder: "inbox", unread: false, subject: "Входящее Письмо 3", from: "Гуру", to: "Jet", date: "23.11.1986"},
+                0: {id: 1, unread: true, subject: "Входящее письмо с очень длинным заголовком", senderName: "Дядя Стёпа Милиционер", receiverName: "Jet", senderId: 1, receiverId: 2, date: "21.11.1986", body: "bod-bod-bod"},
+                1: {id: 2, unread: false, subject: "Входящее Письмо 2", senderName: "Любофф", receiverName: "Jet", senderId: 1, receiverId: 2, date: "22.11.1986", body: "bod-bod-bod"},
+                2: {id: 3, unread: false, subject: "Входящее Письмо 3", senderName: "Гуру", receiverName: "Jet", senderId: 1, receiverId: 2, date: "23.11.1986", body: "bod-bod-bod"},
             },
             outbox: {
-                4: {folder: "outbox", unread: false, subject: "Исходящее Письмо 1", to: "На Берлин", from: "Jet", date: "21.11.1986"},
-                5: {folder: "outbox", unread: true, subject: "Исходящее Письмо 2", to: "Любимой", from: "Jet", date: "22.11.1986"},
-                6: {folder: "outbox", unread: false, subject: "Исходящее Письмо 3", to: "В канцелярию", from: "Jet", date: "23.11.1986"},
+                0: {id: 4, unread: false, subject: "Исходящее Письмо 1", receiverName: "На Берлин", senderName: "Jet", senderId: 1, receiverId: 2, date: "21.11.1986", body: "bod-bod-bod"},
+                1: {id: 5, unread: true, subject: "Исходящее Письмо 2", receiverName: "Любимой", senderName: "Jet", senderId: 1, receiverId: 2, date: "22.11.1986", body: "bod-bod-bod"},
+                2: {id: 6, unread: false, subject: "Исходящее Письмо 3", receiverName: "В канцелярию", senderName: "Jet", senderId: 1, receiverId: 2, date: "23.11.1986", body: "bod-bod-bod"},
             },
             drafts: {
-                7: {folder: "drafts", subject: "Черновик 1", to: "Эксперт", from: "Jet", date: "21.11.1986"},
-                8: {folder: "drafts", subject: "Черновик 2", to: "Любофф", from: "Jet", date: "22.11.1986"},
-                9: {folder: "drafts", subject: "Черновик 3", to: "Гуру", from: "Jet", date: "23.11.1986"},
+                0: {id: 7, subject: "Черновик 1", receiverName: "Эксперт", senderName: "Jet", senderId: 1, receiverId: 2, date: "21.11.1986", body: "bod-bod-bod"},
+                1: {id: 8, subject: "Черновик 2", receiverName: "Любофф", senderName: "Jet", senderId: 1, receiverId: 2, date: "22.11.1986", body: "bod-bod-bod"},
+                2: {id: 9, subject: "Черновик 3", receiverName: "Гуру", senderName: "Jet", senderId: 1, receiverId: 2, date: "23.11.1986", body: "bod-bod-bod"},
             },
             trash: {
-                10: {folder: "inbox", unread: false, subject: "Удалённое Письмо 1", to: "Эксперт", from: "Jet", date: "21.11.1986"},
-                11: {folder: "outbox", unread: false, subject: "Удалённое Письмо 2", to: "Любофф", from: "Jet", date: "22.11.1986"},
-                12: {folder: "drafts", unread: true, subject: "Удалённое Письмо 3", to: "Гуру", from: "Jet", date: "23.11.1986"}
+                0: {id: 10, folder: "inbox", unread: false, subject: "Удалённое Письмо 1", receiverName: "Эксперт", senderName: "Jet", senderId: 1, receiverId: 2, date: "21.11.1986", body: "bod-bod-bod"},
+                1: {id: 11, folder: "outbox", unread: false, subject: "Удалённое Письмо 2", receiverName: "Любофф", senderName: "Jet", senderId: 1, receiverId: 2, date: "22.11.1986", body: "bod-bod-bod"},
+                2: {id: 12, folder: "drafts", unread: true, subject: "Удалённое Письмо 3", receiverName: "Гуру", senderName: "Jet", senderId: 1, receiverId: 2, date: "23.11.1986", body: "bod-bod-bod"}
             }
         }';
 
@@ -128,41 +125,51 @@ class Mail_Controller extends Template_Controller
      * отдаёт информацию о письме и помечает его как прочитанное
      * @return json письмо
      */
-    function mail() {
+    function get() {
+        /*
         $jsonArr = array(
             '{
                 id:'. $_POST['id'] .',
                 folder: "inbox",
                 unread: false,
                 subject: "Калькуляция",
-                from: "Эксперт",
-                to: "Jet",
+                senderName: "Эксперт",
+                receiverName: "Jet",
+                senderId: 1,
+                receiverId: 2,
                 date: "21.11.1986",
-                text: "Greeting from <b>England</b>, sir!<br><br>Good bye."
+                body: "Greeting senderName <b>England</b>, sir!<br><br>Good bye."
             }',
             '{
                 id:'. $_POST['id'] .',
                 folder: "inbox",
                 unread: false,
                 subject: "Респект",
-                from: "Вова Путин",
-                to: "Jet",
+                senderName: "Вова Путин",
+                receiverName: "Jet",
+                senderId: 1,
+                receiverId: 2,
                 date: "21.11.1986",
-                text: "<h1>Здаров, чувак!</h1><br><br>Bye."
+                body: "<h1>Здаров, чувак!</h1><br><br>Bye."
             }',
             '{
                 id:'. $_POST['id'] .',
                 folder: "inbox",
                 unread: false,
                 subject: "Чмоки",
-                from: "Блондинка",
-                to: "Jet",
+                senderName: "Блондинка",
+                receiverName: "Jet",
+                senderId: 1,
+                receiverId: 2,
                 date: "21.11.1986",
-                text: "Greeting from England, <i>sir</i>!<br><br>Kiss you ;)"
+                body: "Greeting senderName England, <i>sir</i>!<br><br>Kiss you ;)"
             }'
         );
 
         die($jsonArr[rand(0, 2)]);
+         */
+
+        die('{result: {text: ""}}');
     }
 
     /**
@@ -176,29 +183,12 @@ class Mail_Controller extends Template_Controller
                 folder: "outbox",
                 unread: false,
                 subject: "'.$_POST['subject'].'",
-                from: "Jet",
-                to: "'.$_POST['to'].'",
+                senderName: "Jet",
+                receiverName: "'.$_POST['receiverName'].'",
+                senderId: 1,
+                receiverId: 2,
                 date: "21.11.1986",
-                text: "'.$_POST['text'].'"
-            }'
-        );
-    }
-
-    /**
-     * создаёт черновик письма
-     * @return json параметры письма
-     */
-    function create_draft(){
-        die(
-            '{
-                id: 20,
-                folder: "drafts",
-                unread: false,
-                subject: "'.$_POST['subject'].'",
-                from: "Jet",
-                to: "'.$_POST['to'].'",
-                date: "21.11.1986",
-                text: "'.$_POST['text'].'"
+                body: "'.$_POST['text'].'"
             }'
         );
     }
@@ -207,36 +197,19 @@ class Mail_Controller extends Template_Controller
      * обновляет текст черновика
      * @return json параметры письма
      */
-    function edit_draft(){
+    function save_draft(){
         die(
             '{
                 id: '.$_POST['id'].',
                 folder: "drafts",
                 unread: false,
                 subject: "newsubj",
-                from: "From",
-                to: "To",
+                senderName: "senderName",
+                receiverName: "receiverName",
+                senderId: 1,
+                receiverId: 2, 
                 date: "21.11.1986",
-                text: "'.$_POST['text'].'"
-            }'
-        );
-    }
-
-    /**
-     * обновляет черновик письма и отправляет его
-     * @return json параметры письма
-     */
-    function send_draft(){
-        die(
-            '{
-                id: '.$_POST['id'].',
-                folder: "outbox",
-                unread: false,
-                subject: "newsubj",
-                from: "From",
-                to: "To",
-                date: "21.11.1986",
-                text: "'.$_POST['text'].'"
+                body: "'.$_POST['text'].'"
             }'
         );
     }
@@ -252,7 +225,7 @@ class Mail_Controller extends Template_Controller
         if(!$param['category'])
             die(0);
         $param['title']=htmlspecialchars($_POST['title']);
-        $param['to']=(int)$_POST['to'];
+        $param['receiverName']=(int)$_POST['receiverName'];
         //$param['date'] = formatRussianDate2MysqlDate($_POST['date']);
         die(json_encode($this->model->add_mail($param)));
     }
@@ -272,7 +245,7 @@ class Mail_Controller extends Template_Controller
      * восстанавливает письмо из папки trash
      * @return 0 1 успешность операции
      */
-    function restore_mail()
+    function restore()
     {
         die('{' . str_replace(',', ': true, ', $_POST['ids']) . ': true}');
         //$param['ids']=explode(',', $_POST['ids']);
