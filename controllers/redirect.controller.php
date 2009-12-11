c21b020bf6b72f329e0d571131da2c7bc2214edd
ukko <ukko@240b7755-b59a-4df0-b20f-7dab6991996d>
2009-12-11 13:28:59 +0000

    Контроллер для редиректов на книгу
    git-svn-id: svn://hm.podzone.net/trunk@1701 240b7755-b59a-4df0-b20f-7dab6991996d

---
+++ controllers/redirect.controller.php
@@ -0,0 +1,19 @@
+<?php if (!defined('INDEX')) trigger_error("Index required!",E_USER_WARNING);
+/**
+ * Контроллер перенаправляющий на внешние ссылки
+ * @copyright http://easyfinance.ru/
+ * @version SVN $Id$
+ */
+
+class Redirect_Controller
+{
+	function index() {}
+
+    /**
+     * Редирект на бесплатную книгу по учёту финансов
+     */
+    function book () {
+        header('Location: http://fir.nes.ru/ru/calendar/PublishingImages/Fingramota%20Web%20Version%20.pdf');
+        exit();
+    }
+}
