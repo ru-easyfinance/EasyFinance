7d9eb4d9d67395a9a4d92a8c603d90f22e84a00e
mamonth <mamonth@240b7755-b59a-4df0-b20f-7dab6991996d>
2009-12-11 17:47:17 +0000

    Счётчики для главной
    git-svn-id: svn://hm.podzone.net/trunk@1731 240b7755-b59a-4df0-b20f-7dab6991996d

--- controllers/welcome.controller.php
+++ controllers/welcome.controller.php
@@ -20,6 +20,24 @@ class Welcome_Controller extends _Core_Controller
 	 */
 	function index()
 	{
+		$counters = array(
+			'users'		=> 8443,
+			'operations'	=> 943132
+		);
+		
+		if( file_exists( SYS_DIR_ROOT . '/include/counters.json' ) )
+		{
+			$countersJson = (array)json_decode( file_get_contents( SYS_DIR_ROOT . '/include/counters.json' ) );
+			
+			if( is_array($countersJson) )
+			{
+				$counters = $countersJson;
+			}
+		}
+		
+		$this->tpl->assign('usersCount',  number_format($counters['users'], 0, ',', ' '));
+		$this->tpl->assign('operationsCount', number_format($counters['operations'], 0, ',', ' '));
+		
 		$welcome = new Welcome_Model();
 		
 		$this->tpl->assign('name_page', 'welcome');
