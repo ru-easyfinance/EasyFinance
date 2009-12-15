ce25afe62247a34fcdc2fc1de0496e3658802d0d
rewle <rewle@240b7755-b59a-4df0-b20f-7dab6991996d>
2009-12-15 08:41:02 +0000

    регистрация - мелкие правки.
    git-svn-id: svn://hm.podzone.net/trunk@1783 240b7755-b59a-4df0-b20f-7dab6991996d

--- www/js/registration.js
+++ www/js/registration.js
@@ -1,92 +1,86 @@
 $(document).ready(function() {
-	/*
-	 * @deprecated Удалить когда появится общая функция работы с хэшем
-	 */
-	var hash = window.location.hash;
-	if (!hash) {hash = '';}
-	//////////////////////////////////////////////////////////////////////
-	switch (hash)
-	{
-		case '#activate':
-			$('#registration').hide();
-			$('#activation').show();
-			break;
-		default :
-			/**
-			 * Проверка полей формы
-			 */
-			$("#formRegister").validate({
-				rules: {
-					name: "required",
-					login: "required",
-					passw: {
-						required: true,
-						minlength: 5
-					},
-					confirm_password: {
-						required: true,
-						minlength: 5,
-						equalTo: $("#passw")
-					},
-					mail: {
-						required: true,
-						email: true
-					}
-				},
-				messages: {
-					name: "Вы не ввели имя!",
-					login: "Вы не ввели логин!",
-					passw: {
-						required: "Вы не ввели пароль!",
-						minlength: "Ваш пароль должен состоять как минимум из 5 символов"
-					},
-					confirm_password: {
-						required: "Вы не ввели пароль!",
-						minlength: "Ваш пароль должен состоять как минимум из 5 символов",
-						equalTo: "Пожалуйста, введите тот же пароль, что и выше"
-					},
-					mail: "Пожалуйста, введите правильный адрес электронной почты!"
-				}
-			});
-			$('#butt').click($("#formRegister").validate);
-			$("#password").blur(function() {
-				$("#confirm_password").valid();
-			});
-			/**
-			 * пересылка данных из формы на сервер
-			 */
-			$('#butt').click(function()
-			{
-				if ($("#formRegister").valid()) {
-					$.post(
-						"/registration/new_user/",
-						{
-							password: $('#passw').val(),
-							confirm_password: $('#confirm_password').val(),
-							login: $('#log').val(),
-							name: $('#name').val(),
-							mail: $('#mail').val()
-						},
-						function (data){
-							if (data && (data.errors == 'succes'))
-							{
-								$('.formRegister').hide();
-								$('#formConfirm').show();	
-							}
-							else
-							{
-								var str = '<ul>'
-								for (var key in data.errors)
-								{
-									str += '<li>' + data.errors[key] + '</li>'
-								}
-								$.jGrowl('<div><b>При регистации возникли ошибки :</b></div>'+str+'</ul>',{theme:'red'})
-							}
-						},
-						'json')
-				}
-			    
-			})
-			break;
+    var hash = window.location.hash;
+    if (!hash) {hash = '';}
+    //////////////////////////////////////////////////////////////////////
+    switch (hash){
+        case '#activate':
+            $('#registration').hide();
+            $('#activation').show();
+            break;
+        default :
+            /**
+             * Условия валидации формы
+             */
+            $("#formRegister").validate({
+                rules: {
+                    name: "required",
+                    login: "required",
+                    passw: {
+                        required: true,
+                        minlength: 5
+                    },
+                    confirm_password: {
+                        required: true,
+                        minlength: 5,
+                        equalTo: $("#passw")
+                    },
+                    mail: {
+                        required: true,
+                        email: true
+                    }
+                },
+                messages: {
+                    name: "Вы не ввели имя!",
+                    login: "Вы не ввели логин!",
+                    passw: {
+                        required: "Вы не ввели пароль!",
+                        minlength: "Ваш пароль должен состоять как минимум из 5 символов"
+                    },
+                    confirm_password: {
+                        required: "Вы не ввели пароль!",
+                        minlength: "Ваш пароль должен состоять как минимум из 5 символов",
+                        equalTo: "Пожалуйста, введите тот же пароль, что и выше"
+                    },
+                    mail: "Пожалуйста, введите правильный адрес электронной почты!"
+                }
+            });
+            /*
+             * Евенты, вызывающие валидацию
+             */
+            $('#butt').click($("#formRegister").validate);
+            $("#password").blur(function() {
+                $("#confirm_password").valid();
+            });
+            /**
+             * пересылка данных из формы на сервер
+             */
+            $('#butt').click(function(){
+                if ($("#formRegister").valid()) {
+                    $('.formRegister').hide();
+                    $.post(
+                        "/registration/new_user/",
+                        {
+                            password: $('#passw').val(),
+                            confirm_password: $('#confirm_password').val(),
+                            login: $('#log').val(),
+                            name: $('#name').val(),
+                            mail: $('#mail').val()
+                        },
+                        function (data){
+                            if (!data || (!data.errors)){
+                                $('#formConfirm').show();
+                            }else{
+                                $('.formRegister').show();
+                                var str = '<ul>'
+                                for (var key in data.errors){
+                                    str += '<li>' + data.errors[key] + '</li>'
+                                }
+                                $.jGrowl('<div><b>При регистации возникли ошибки :</b></div>' + str + '</ul>' , {theme: 'red'})
+                            }
+                        },
+                        'json')
+                    }
+                })
+                break;
 	}
 });
