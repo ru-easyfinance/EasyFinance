59ac4e560a8e1073ba8bf002f0f656057ff347f3
serj <serj@240b7755-b59a-4df0-b20f-7dab6991996d>
2010-01-12 17:26:53 +0000

    ""
    git-svn-id: svn://hm.podzone.net/trunk@2188 240b7755-b59a-4df0-b20f-7dab6991996d

---
+++ controllers/invest.controller.php
@@ -0,0 +1,33 @@
+<?php if (!defined('INDEX')) trigger_error("Index required!",E_USER_WARNING);
+/**
+ * Класс контроллера для модуля инвестиционных активов пользователя
+ * @copyright http://easyfinance.ru/
+ * @version SVN $Id: invest.controller.php 2050 2009-12-25 16:10:20Z kiselev $
+ */
+
+class Invest_Controller extends _Core_Controller_UserCommon
+{
+
+    /**
+     * Ссылка на класс User
+     * @var User
+     */
+    private $user = null;
+
+    /**
+     * Ссылка на класс модель
+     * @var Invest_Model
+     */
+    private $model = null;	
+
+    /**
+     * Конструктор класса
+     * @return void
+     */
+    function __init()
+    {
+        $this->user  = Core::getInstance()->user;
+        $this->model = new Invest_Model();
+        $this->tpl->assign('name_page', 'invest/portfolio');
+    }
+}
