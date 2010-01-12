bde92ec45cfe4408c17306d182af81147de38382
serj <serj@240b7755-b59a-4df0-b20f-7dab6991996d>
2010-01-12 18:04:01 +0000

    ""
    git-svn-id: svn://hm.podzone.net/trunk@2194 240b7755-b59a-4df0-b20f-7dab6991996d

---
+++ models/invest.model.php
@@ -0,0 +1,30 @@
+<?php if (!defined('INDEX')) trigger_error("Index required!",E_USER_WARNING);
+/**
+ * Класс модели для управления инвестиционными активами пользователя
+ * @copyright http://easyfinance.ru/
+ * @version SVN $Id: $$
+ */
+class Invest_Model
+{
+    /**
+     * Ссылка на экземпляр DBSimple
+     * @var DbSimple_Mysql
+     */
+    private $db = NULL;
+
+    /**
+     * Ид текущего пользователя
+     * @var int
+     */
+    private $user_id = NULL;
+	
+    /**
+     * Конструктор
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->db = Core::getInstance()->db;
+        $this->user_id = Core::getInstance()->user->getId();
+    }
+}
