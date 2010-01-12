a523de1a70a0d5b2700e707d5e3f16a99e4d910b
challenger <challenger@240b7755-b59a-4df0-b20f-7dab6991996d>
2010-01-12 13:12:50 +0000

    апдейт базы + доп поле для банка, точнее гос валюты, которая однозгачно его определяет
    git-svn-id: svn://hm.podzone.net/trunk@2170 240b7755-b59a-4df0-b20f-7dab6991996d

--- scheme/versions/37.sql
+++ scheme/versions/37.sql
@@ -1,1 +1,3 @@
-UPDATE `currency` SET cur_char_code='AZN' WHERE cur_char_code='AZM'
+ALTER TABLE `daily_currency` ADD `currency_from` INT( 11 ) DEFAULT '1' NOT NULL AFTER `currency_id` ;
+
+UPDATE `currency` SET cur_char_code='AZN' WHERE cur_char_code='AZM';
