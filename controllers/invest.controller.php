f768fde457c6f1aabf25e9a62f041def4df96681
serj <serj@240b7755-b59a-4df0-b20f-7dab6991996d>
2010-01-12 18:45:42 +0000

    ""
    git-svn-id: svn://hm.podzone.net/trunk@2199 240b7755-b59a-4df0-b20f-7dab6991996d

--- controllers/invest.controller.php
+++ controllers/invest.controller.php
@@ -30,4 +30,27 @@ class Invest_Controller extends _Core_Controller_UserCommon
         $this->model = new Invest_Model();
         $this->tpl->assign('name_page', 'invest/portfolio');
     }
+    
+    /**
+     * Индексная страница
+     * @param $args array mixed
+     * @return void
+     */
+    function index()
+    {
+        $cur=Core::getInstance()->user->getUserCurrency();
+        $cur_k=array_shift($cur);
+        $this->tpl->assign("page_title", "account all");
+        $this->tpl->assign('accounts', Core::getInstance()->user->getUserAccounts());
+        $this->tpl->assign('type_accounts', $this->model->getTypeAccounts());
+        $this->tpl->assign("template", "default");
+        $this->tpl->assign("cur", json_encode($cur_k['abbr']));
+
+        // Операция
+        $this->tpl->assign('category', get_tree_select());
+        $targets = new Targets_Model();
+        //$this->tpl->assign('targetList', $targets->getLastList(0, 100));
+
+
+    }
 }
