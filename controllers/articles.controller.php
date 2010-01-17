033a370522c8e03eef517f865f84ec98f758d0f4
mamonth <mamonth@240b7755-b59a-4df0-b20f-7dab6991996d>
2010-01-17 16:29:51 +0000

    Модуль Статьи, первоначальная реализация
    git-svn-id: svn://hm.podzone.net/trunk@2342 240b7755-b59a-4df0-b20f-7dab6991996d

---
+++ controllers/articles.controller.php
@@ -0,0 +1,86 @@
+<?php if (!defined('INDEX')) trigger_error("Index required!",E_USER_WARNING);
+
+/**
+ * Статьи
+ * @category articles
+ * @copyright http://easyfinance.ru/
+ * @author Andrew Tereshko aka mamonth
+ */
+class Articles_Controller extends _Core_Controller
+{
+	/**
+	 * Конструктор класса
+	 * @return void
+	 */
+	protected function __init()
+	{	
+		$this->tpl->assign('no_menu', '1');
+	}
+	
+	function index( $args = array() )
+	{
+		$articleId = false;
+		
+		if( is_array($args) && array_key_exists(0,$args) && is_numeric( $args[0] ) )
+		{
+			$articleId = (int)$args[0];
+		}
+		
+		if( $articleId )
+		{
+			$article = Article::load( $articleId );
+			
+			$this->tpl->assign( 'article', $article->getArray() );
+		
+			$this->tpl->assign('name_page', 'articles/page');
+		}
+		// Если не передан ид статьи - возвращаем первую страницу
+		else
+		{
+			$this->page();
+		}
+	}
+	
+	function page( $args = array() )
+	{
+		if( !isset($args) || !is_array($args) || !isset($args[0]) )
+		{
+			$pageNum = 1;
+		}
+		else
+		{
+			$pageNum = (int)$args[0];
+		}
+		
+		$itemsPerPage = 20;
+		
+		$itemsTotal = Article_Model::loadTotalNum();
+		
+		$pagesTotal = ceil( $itemsTotal / $itemsPerPage );
+		
+		if( $pageNum > $pagesTotal || $pageNum < 1)
+		{
+			//throw 404
+			header('Location: /articles');
+		}
+		
+		$container = Article_Collection::loadRange( ($pageNum - 1) * $itemsPerPage , $itemsPerPage );
+		
+		$this->tpl->assign( 'articles', $container->getArray() );
+		
+		$this->tpl->assign('name_page', 'articles/list');
+	}
+	
+	function __call( $method, $args)
+	{
+		if( method_exists( $this, $method) )
+		{
+			call_user_method( $method, $this, $args );
+		}
+		else
+		{
+			array_unshift($args, $method);
+			call_user_method('index', $this, $args);
+		}
+	}
+}
