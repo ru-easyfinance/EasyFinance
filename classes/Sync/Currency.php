80b7b9be208e105a00ab1bc7a6f172fc79b07bae
challenger <challenger@240b7755-b59a-4df0-b20f-7dab6991996d>
2010-01-18 15:27:30 +0000

    поправил синхронизацию
    git-svn-id: svn://hm.podzone.net/trunk@2380 240b7755-b59a-4df0-b20f-7dab6991996d

---
+++ classes/Sync/Currency.php
@@ -0,0 +1,37 @@
+<?php
+
+class Currency {
+    private $db = null;
+    private $user = null;
+
+    /**
+     * конструктор инициализирует юзера и бд
+     * @param integer $id
+     * @param integer $db
+     */
+    function __construct($id, $db){
+        $this->db = $db;
+        $this->user = $id;
+    }
+
+
+
+    function FormArray( &$data=''){
+        $sql = "SELECT user_currency_list AS li, user_currency_default AS def FROM users WHERE id = ?";
+        $li = $this->db->query($sql, $this->user);
+        $cur_user_string = $li[0]['li'];
+        $cur_user_array = unserialize($cur_user_string);
+        $mas = "'".implode("','", $cur_user_array)."'";
+        $sql = "SELECT c.cur_id as id,  c.cur_char_code as charCode, c.cur_name as abbr
+            FROM currency c
+            WHERE c.cur_id IN ($mas)";
+        $li = $this->db->query($sql);
+
+        foreach ($li as $key=>$v){
+            $data[7][0]['tablename'] = 'Currencies';
+                $data[7][$key+1]['easykey'] = $key;
+                $data[7][$key+1]['charCode'] = $v['charCode'];
+        }
+    }
+    
+}
