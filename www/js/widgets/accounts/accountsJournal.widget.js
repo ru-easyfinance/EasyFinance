d69646457f3a47af56935fd56c8e404b874155cc
challenger <challenger@240b7755-b59a-4df0-b20f-7dab6991996d>
2010-01-18 10:39:37 +0000

    журнал операций
    git-svn-id: svn://hm.podzone.net/trunk@2361 240b7755-b59a-4df0-b20f-7dab6991996d

--- www/js/widgets/accounts/accountsJournal.widget.js
+++ www/js/widgets/accounts/accountsJournal.widget.js
@@ -253,9 +253,9 @@ easyFinance.widgets.accountsJournal = function(){
                 str = '<tr class="item" id="accountsJournalAcc_' + account_list[key]['id'] + '">';
                 str = str + '<td class="name">' + account_list[key]["name"] + '</td>';
                 if (type == 2) //для долга печатаем с противоположным знаком
-                    str = str + '<td class="totalBalance ' + colorClass + '">' + formatCurrency(-account_list[key]["totalBalance"] * curr[account_list[key]["currency"]]['cost'] / curr[num]['cost']) + '</td>';
+                    str = str + '<td class="totalBalance ' + colorClass + '">' + formatCurrency(-account_list[key]["totalBalance"] ) + '</td>';
                 else
-                    str = str + '<td class="totalBalance ' + colorClass + '">' + formatCurrency(account_list[key]["totalBalance"] * curr[account_list[key]["currency"]]['cost'] / curr[num]['cost']) + '</td>';
+                    str = str + '<td class="totalBalance ' + colorClass + '">' + formatCurrency(account_list[key]["totalBalance"] ) + '</td>';
 
                 str = str + '<td class="cur">' + res.currency[account_list[key]["currency"]]['text'] + '</td>';
                 if (type == 2)//для долга выводим с противоположным знаком
