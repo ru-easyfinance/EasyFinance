fb54ae439eb68a44c7b7ff9d13480900f45ef846
rewle <rewle@240b7755-b59a-4df0-b20f-7dab6991996d>
2010-01-21 09:45:18 +0000

    Страница счетов - первая рабочая версия
    git-svn-id: svn://hm.podzone.net/trunk@2456 240b7755-b59a-4df0-b20f-7dab6991996d

--- www/admin/articles/js/list.js
+++ www/admin/articles/js/list.js
@@ -1,6 +1,37 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
+$(document).ready(function(){
+//    <table>
+//                  <tbody>
+//                      <tr>
+//                          <td class="date">19-03-2007</td>
+//                          <td class="title">Тестовая статья</td>
+//                          <td class="status">Опубликована</td>
+//                          <td class="remove"> Удалить </td>
+//                          <td class="edit"> Редактировать </td>
+//                          <td class="public event"> Опубликовать </td>
+//                      </tr>
+//                  </tbody>
+//              </table>
+//    if(typeof res != 'Array' || typeof res != 'Object'){
+//        return false;
+//    }
+    var htmlArticle = '<table>'
+    for(var key in res){
+        if (typeof res.key == 'Object'){
+            htmlArticle += '<tr>'
+                + '<td class="date">' + (res[key].date||'! Нет !') + '</td>'
+                + '<td class="title">' + (res[key].title||'') + '</td>'
+                + '<td class="status">' + (res[key].status ? 'Опубликована' : 'Не опубликована') + '</td>'
+                + '<td class="remove"><a href="index.php?page=articleDel&id=' + (res[key].id||'0') + '"> Удалить </a></td>'
+                + '<td class="edit"><a href="index.php?page=editor&id=' + (res[key].id||'0') + '"> Редактировать </a></td>'
+            if (res[key].status){
+                htmlArticle += '<td class="public event"> &nbsp; </td>';
+            }else{
+                htmlArticle += '<td class="public"><a href="index.php?page=public&id=' + (res[key].id||'0') + '"> Публиковать </a></td>';
+            }
+            htmlArticle += '</tr>';
+        }
+    }
+    htmlArticle = '</table>'
+    $('div.list div.body').html(htmlArticle);
+});
 
