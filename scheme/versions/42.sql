15bd5508b899d1ffed97f175ce0ee001c1901720
challenger <challenger@240b7755-b59a-4df0-b20f-7dab6991996d>
2010-01-21 15:18:42 +0000

    апдейт базы для статей
    git-svn-id: svn://hm.podzone.net/trunk@2479 240b7755-b59a-4df0-b20f-7dab6991996d

---
+++ scheme/versions/42.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `images` (
+`id` INT NOT NULL ,
+`parent_id` INT NOT NULL ,
+`path` TEXT NOT NULL ,
+`url` TEXT NOT NULL ,
+PRIMARY KEY ( `id` )
+) COMMENT = 'таблица ссылок на картинки';
+CREATE TABLE `images_articles` (
+`image_id` INT NOT NULL ,
+`article_id` INT NOT NULL
+) COMMENT = 'связь статей и изображений';
