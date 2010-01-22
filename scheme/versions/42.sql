ee0a08e9d5807fe2e1a7233861bb6a08c004805e
challenger <challenger@240b7755-b59a-4df0-b20f-7dab6991996d>
2010-01-22 14:46:30 +0000

    коммичу апдейт базы
    git-svn-id: svn://hm.podzone.net/trunk@2523 240b7755-b59a-4df0-b20f-7dab6991996d

--- scheme/versions/42.sql
+++ scheme/versions/42.sql
@@ -1,13 +1,16 @@
-CREATE TABLE `images` (
+CREATE TABLE IF NOT EXISTS `images` (
 `id` INT NOT NULL ,
 `parent_id` INT NOT NULL ,
 `path` TEXT NOT NULL ,
 `url` TEXT NOT NULL ,
 PRIMARY KEY ( `id` )
 ) COMMENT = 'таблица ссылок на картинки';
-CREATE TABLE `images_articles` (
+
+CREATE TABLE IF NOT EXISTS `images_articles` (
 `image_id` INT NOT NULL ,
 `article_id` INT NOT NULL
 ) COMMENT = 'связь статей и изображений';
+
 ALTER TABLE `images` CHANGE `id` `id` INT( 11 ) AUTO_INCREMENT;
+
 ALTER TABLE `articles` ADD `image_id` INT NOT NULL ;
