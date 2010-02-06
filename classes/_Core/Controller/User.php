9963ddd233a5f2808c8476a5462576d7e843c6cc
mamonth <mamonth@240b7755-b59a-4df0-b20f-7dab6991996d>
2010-02-06 08:13:48 +0000

    related to prev commit
    git-svn-id: svn://hm.podzone.net/branches/engine2@2880 240b7755-b59a-4df0-b20f-7dab6991996d

--- classes/_Core/Controller/User.php
+++ classes/_Core/Controller/User.php
@@ -2,7 +2,7 @@
 
 abstract class _Core_Controller_User extends _Core_Controller
 {
-	public function __construct( $templateEngine )
+	public function __construct( $templateEngine, _Core_Request $request)
 	{
 		//Проверяем авторизован ли пользователь. Если нет - редиректим на логин
 		if( !Core::getInstance()->user->getId() )
@@ -10,6 +10,6 @@ abstract class _Core_Controller_User extends _Core_Controller
 			header( 'Location: /login/' );
 		}
 		
-		parent::__construct( $templateEngine );
+		parent::__construct( $templateEngine, $request );
 	}
 }
