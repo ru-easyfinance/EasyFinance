ccd253cd06ab0ed1a3a4b421700acd2bd5bb8e43
ukko <ukko@240b7755-b59a-4df0-b20f-7dab6991996d>
2010-02-12 12:21:11 +0000

    Скрипт для повторного подтверждение регистрации - всё готово.
    git-svn-id: svn://hm.podzone.net/trunk@3058 240b7755-b59a-4df0-b20f-7dab6991996d

--- scripts/resendRegMail.php
+++ scripts/resendRegMail.php
@@ -6,17 +6,24 @@ define ('INDEX', true);
 require_once dirname(dirname(__FILE__)) . "/core/external/Swift/swift_required.php";
 require_once dirname(dirname(__FILE__)) . "/include/config.php";
 
-$mailTransport = Swift_SendmailTransport::newInstance('/usr/sbin/sendmail -bs');
+//$mailTransport = Swift_SendmailTransport::newInstance('/usr/sbin/sendmail -bs');
+//$mailTransport = Swift_MailTransport::newInstance();
+// smtp
+$mailTransport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'ssl')
+	->setUsername('info@easyfinance.ru')
+	->setPassword('j2df32nD3l7sFa2');
+	//->setUsername('support@easyfinance.ru')
+	//->setPassword('7uN3BN6t');
 
 $mailer = Swift_Mailer::newInstance( $mailTransport );
-
-$mailer->registerPlugin(new Swift_Plugins_LoggerPlugin(new Swift_Plugins_Loggers_EchoLogger()));
+$logger = new Swift_Plugins_Loggers_EchoLogger();
+$mailer->registerPlugin(new Swift_Plugins_LoggerPlugin( $logger ));
 
 
 // Получаем список пользователей, которым нужно отправить регистрацию
 
 // Дата от которой нужно получить список пользователей
-$date = "2010-02-12";
+$date = "2009-02-12";
 
 
 $dsn      = 'mysql:dbname='.SYS_DB_BASE.';host=127.0.0.1';
@@ -26,13 +33,11 @@ $password = SYS_DB_PASS;
 $mysql = mysql_connect(SYS_DB_HOST, SYS_DB_USER, SYS_DB_PASS);
 mysql_select_db(SYS_DB_BASE, $mysql);
 
-$unregistredUsers = mysql_query("SELECT * FORM users WHERE user_active=0 AND user_created >= '{$date}';", $mysql);
-
-die(print_r($unregistredUsers));
-
 $ids = '';
-foreach ( $unregistredUsers as $k => $unUser ) {
-    $body = "Здравствуйте, {$unUser['user_name']}!\n
+$result = mysql_query("SELECT * FROM users WHERE user_active=0 AND user_created >= '{$date}';", $mysql);
+while ($unUser = mysql_fetch_array($result, MYSQL_ASSOC)) {
+
+    $body = "\nЗдравствуйте, {$unUser['user_name']}!\n
         Ваш e-mail был указан при регистрации в системе.\n\n
 
         Ваша учётная запись была активирована.\n
@@ -55,12 +60,13 @@ foreach ( $unregistredUsers as $k => $unUser ) {
         ->setBody($body, 'text/plain');
     // Отсылаем письмо
 
-    $result = Core::getInstance()->mailer->send($message);
+    $mailer->send($message);
 
     if ( !empty ($ids)) { $ids .= ','; }
     $ids .= $unUser['id'];
 
-    $mysqli->query("UPDATE users SET user_active=1 , user_new=0 WHERE id IN ( {$ids} )");
+    mysql_query("UPDATE users SET user_active=1 , user_new=0 WHERE id IN ( {$ids} )", $mysql);
 }
 
 echo $logger->dump();
+mysql_close($mysql);
