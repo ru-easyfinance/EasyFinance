e29c60dceac806e6c293ac11cef018aa5d092ed5
ukko <ukko@240b7755-b59a-4df0-b20f-7dab6991996d>
2010-03-11 12:41:01 +0000

    Мерж с бранча (2)
    git-svn-id: svn://hm.podzone.net/trunk@3471 240b7755-b59a-4df0-b20f-7dab6991996d

--- configs/access.php
+++ configs/access.php
@@ -19,33 +19,34 @@
 
 $accessConfig = array(
 	// индекс.
-	'review' 	=> _Core_Access::ALLOW_ALL,
+	'review'        => _Core_Access::ALLOW_ALL,
 	'rules'         => _Core_Access::ALLOW_ALL,
 	'about'         => _Core_Access::ALLOW_ALL,
 	'security'      => _Core_Access::ALLOW_ALL,
 	'rules'         => _Core_Access::ALLOW_ALL,
-	'feedback' 	=> _Core_Access::ALLOW_ALL,
-	'articles' 	=> _Core_Access::ALLOW_ALL,
-	'login' 	=> _Core_Access::ALLOW_ALL,
-	'logout' 	=> _Core_Access::ALLOW_ALL,
+	'feedback'      => _Core_Access::ALLOW_ALL,
+	'articles'      => _Core_Access::ALLOW_ALL,
+	'login'         => _Core_Access::ALLOW_ALL,
+	'logout'        => _Core_Access::ALLOW_ALL,
 	'registration' 	=> _Core_Access::ALLOW_ALL,
-	'restore' 	=> _Core_Access::ALLOW_ALL,
+	'restore'       => _Core_Access::ALLOW_ALL,
 	'redirect'      => _Core_Access::ALLOW_ALL,
 	
-	'profile' 	=> _Core_Access::ALLOW_AUTHORIZED,
+	'profile'       => _Core_Access::ALLOW_AUTHORIZED,
 	
-	'info' 		=> array( _User::TYPE_COMMON, _User::TYPE_PRO ),
-	'report' 	=> array( _User::TYPE_COMMON, _User::TYPE_PRO ),
-	'accounts' 	=> array( _User::TYPE_COMMON, _User::TYPE_PRO ),
+	'info'          => array( _User::TYPE_COMMON, _User::TYPE_PRO ),
+	'report'        => array( _User::TYPE_COMMON, _User::TYPE_PRO ),
+	'accounts'      => array( _User::TYPE_COMMON, _User::TYPE_PRO ),
 	'operation' 	=> array( _User::TYPE_COMMON, _User::TYPE_PRO ),
-	'category' 	=> array( _User::TYPE_COMMON, _User::TYPE_PRO ),
-	'budget' 	=> array( _User::TYPE_COMMON, _User::TYPE_PRO ),
-	'targets' 	=> array( _User::TYPE_COMMON, _User::TYPE_PRO ),
-	'calendar' 	=> array( _User::TYPE_COMMON, _User::TYPE_PRO ),
-	'periodic' 	=> array( _User::TYPE_COMMON, _User::TYPE_PRO ),
+	'category'      => array( _User::TYPE_COMMON, _User::TYPE_PRO ),
+	'budget'        => array( _User::TYPE_COMMON, _User::TYPE_PRO ),
+	'targets'       => array( _User::TYPE_COMMON, _User::TYPE_PRO ),
+	'calendar'      => array( _User::TYPE_COMMON, _User::TYPE_PRO ),
+	'periodic'      => array( _User::TYPE_COMMON, _User::TYPE_PRO ), //@TODO Позже удалить
+    'tags'          => array( _User::TYPE_COMMON, _User::TYPE_PRO ),
 	
-	'experts'	=> _User::TYPE_PRO,
+	'experts'       => _User::TYPE_PRO,
 	
-	'expert' 	=> _User::TYPE_EXPERT,
+	'expert'        => _User::TYPE_EXPERT,
 	
 );
