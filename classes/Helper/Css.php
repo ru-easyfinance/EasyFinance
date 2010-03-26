4c3a44aa384189b3db83e1f84652a6173e66e5c7
Alexandr Ilychev <rewle@ya.ru>
2010-03-26 16:20:45 +0300

    Правка в подгрузке цсс

--- classes/Helper/Css.php
+++ classes/Helper/Css.php
@@ -12,8 +12,6 @@ class Helper_Css implements _Core_Router_iHook
 		else
 		{
 			$templateEngine->append('css', 'main.css');
-			$templateEngine->append('css', 'jquery/south-street/ui.all.css');
-			//$tpl->append('css', 'jquery/south-street/ui.base.css');
 			$templateEngine->append('css', 'jquery/south-street/ui.core.css');
 			$templateEngine->append('css', 'jquery/south-street/ui.resizable.css');
 			$templateEngine->append('css', 'jquery/south-street/ui.dialog.css');
@@ -22,29 +20,27 @@ class Helper_Css implements _Core_Router_iHook
 			$templateEngine->append('css', 'jquery/jquery.rw-calculator.css');
 			$templateEngine->append('css', 'jquery/jquery.jgrowl.css');
 			$templateEngine->append('css', 'jquery/fullcalendar.css');
+		    $templateEngine->append('css', 'dialogBox.css');
+			$templateEngine->append('css', 'menuUser.css');
+			$templateEngine->append('css', 'efTable.css');
+	        $templateEngine->append('css', 'calendar.css');
+			$templateEngine->append('css', 'jquery/jHtmlArea.css');
+			$templateEngine->append('css', 'jquery/jHtmlArea.ColorPickerMenu.css');
+			$templateEngine->append('css', 'jquery/fancy.css');
+			$templateEngine->append('css', 'jquery/sexyCombo.css');
+			$templateEngine->append('css', 'calendarOverdue.css');
+			$templateEngine->append('css', 'calendarLeft.css');
+			$templateEngine->append('css', 'report.css');
+			$templateEngine->append('css', 'expert.css');
+			$templateEngine->append('css', 'expertsList.css');
+			$templateEngine->append('css', 'profile.css');
+	        $templateEngine->append('css', 'categoriesJournal.css');
+			$templateEngine->append('css', 'operationsJournal.css');
+			$templateEngine->append('css', 'budgetMaster.css');
+			$templateEngine->append('css', 'budget.css');
+	        $templateEngine->append('css', 'accountsPanel.css');
+	        $templateEngine->append('css', 'accountsJournal.css');
 		}
 
-        $templateEngine->append('css', 'dialogBox.css');
-		$templateEngine->append('css', 'menuUser.css');
-		$templateEngine->append('css', 'efTable.css');
-                $templateEngine->append('css', 'calendar.css');
-		$templateEngine->append('css', 'jquery/jHtmlArea.css');
-		$templateEngine->append('css', 'jquery/jHtmlArea.ColorPickerMenu.css');
-		
-		$templateEngine->append('css', 'jquery/fancy.css');
-		
-		$templateEngine->append('css', 'jquery/sexyCombo.css');
-		$templateEngine->append('css', 'calendarOverdue.css');
-                $templateEngine->append('css', 'calendarLeft.css');
-		$templateEngine->append('css', 'report.css');
-		$templateEngine->append('css', 'expert.css');
-		$templateEngine->append('css', 'expertsList.css');
-		$templateEngine->append('css', 'profile.css');
-        $templateEngine->append('css', 'categoriesJournal.css');
-		$templateEngine->append('css', 'operationsJournal.css');
-		$templateEngine->append('css', 'budgetMaster.css');
-		$templateEngine->append('css', 'budget.css');
-                $templateEngine->append('css', 'accountsPanel.css');
-                $templateEngine->append('css', 'accountsJournal.css');
-	}
+    }
 }
