f29908a1ef5616f76f423de677e12d6b7a9b8808
Andrey Zharikov <jet@playz.ru>
2010-03-26 13:59:26 +0300

    #1056 Доработать календарь в правой панели
    Убрал моргание диалога при добавлении операции

--- www/js/widgets/calendar/calendarPreview.widget.js
+++ www/js/widgets/calendar/calendarPreview.widget.js
@@ -103,6 +103,8 @@ easyFinance.widgets.calendarPreview = function(){
                 time: ''//dt.toLocaleTimeString().substr(0, 5)
             };
             easyFinance.widgets.operationEdit.fillFormCalendar(data, false, true);
+
+            return false;
         });
         
     }
