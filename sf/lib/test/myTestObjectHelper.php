1fe42929cd7cc670425d375a996d1dce01ea7d78
Maxim Oleinik <maxim.oleinik@gmail.com>
2010-03-31 14:59:51 +0400

    #1079 Import AMT: Форма для импорта операций

--- sf/lib/test/myTestObjectHelper.php
+++ sf/lib/test/myTestObjectHelper.php
@@ -5,4 +5,18 @@
  */
 class myTestObjectHelper extends sfPHPUnitObjectHelper
 {
+    /**
+     * Создать пользователя
+     */
+    public function makeUser(array $props = array(), $save = true)
+    {
+        $defaultProps = array(
+            'user_service_mail' => sprintf('user%d@example.org', $this->getUniqueCounter()),
+        );
+        $props = array_merge($defaultProps, $props);
+
+        $ob = $this->makeModel('User', $props);
+        return $ob;
+    }
+
 }
