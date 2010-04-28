8c807b38c0d4d0de6336778bc983f15cbb042418
Andrey Zharikov <jet@playz.ru>
2010-04-28 16:04:27 +0400

    t1231 интеграция: внедрение блока регистрации

--- www/js/screens/integration.screen.js
+++ www/js/screens/integration.screen.js
@@ -4,6 +4,17 @@ $(function() {
         event: ""
     });
 
+    var _$dlgRegistration = $("#registration").dialog({
+        title: "Регистрация",
+        bgiframe: true,
+        autoOpen: false,
+        modal:true
+    });
+
+    $("#btnRegister").click(function() {
+        _$dlgRegistration.dialog('open');
+    });
+
     /* // @TEST
     if (document.location.pathname.indexOf("integration") != -1) {
         $.jGrowl("Регистрация успешно завершена!", {theme: 'green'});
