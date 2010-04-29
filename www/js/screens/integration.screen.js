fdd8dbe8d0f1b28e897ed2abb6762dd659a4cf8c
Andrey Zharikov <jet@playz.ru>
2010-04-29 16:54:10 +0400

    t1242 интеграция: создание email

--- www/js/screens/integration.screen.js
+++ www/js/screens/integration.screen.js
@@ -26,6 +26,9 @@ $(function() {
         _$dlgAuthentication.dialog('open');
     });
 
+	easyFinance.widgets.userIntegrations.init();
+	easyFinance.widgets.userIntegrations.load(res.profile.integration.email && res.profile.integration.email != '' ? {service_mail : res.profile.integration.email} : {});
+
     /* // @TEST
     if (document.location.pathname.indexOf("integration") != -1) {
         $.jGrowl("Регистрация успешно завершена!", {theme: 'green'});
