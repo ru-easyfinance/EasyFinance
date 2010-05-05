0e37aae841b317cda840366981eeb8bf914b83b9
Svel <Svel.Sontz@gmail.com>
2010-05-06 02:45:20 +0400

    Базовый шаблон: колонки

--- sf/apps/frontend/templates/layout.php
+++ sf/apps/frontend/templates/layout.php
@@ -36,7 +36,16 @@
                 <ul class="menu2">
                         <!--- <li><a href="/info/">Личный кабинет</a></li> --->
                         <!--- <li><a href="/profile/">Настройки профиля</a></li> --->
-                        <li><a href="/profile/"><?php echo 'Узернэйм'; //{$user_info.user_name} ?>&nbsp;</a></li>
+                        <li><a href="/profile/"><?php
+                        session_write_close();
+                        session_name('PHPSESSID');
+                        session_start();
+
+                        echo $_SESSION['user']['user_name'];
+                        session_write_close();
+                        session_name('symfony');
+                        session_start();
+                        ?>&nbsp;</a></li>
                         <li><a id="show_logout" href="/logout/" title="Выход">ВЫХОД</a></li>
                 </ul>
             </div>
@@ -62,7 +71,270 @@
                     <!--/реклама-->
             </div>
 
-        <?php echo $sf_content ?>
+
+<!--середина-->
+
+    <!-- mainmenu -->
+    <div class="menu3">
+        <ul class="dropdown">
+            <li id="m1">
+                <a href="/info/"></a>
+                <ul>
+                    <li><span> </span><a href="/info/">Инфо-панель</a></li>
+                    <li><span> </span><a href="/profile/">Профиль</a></li>
+                    <li class="last"></li>
+                </ul>
+            </li>
+            <li id="m2">
+                <a href="/accounts/"></a>
+                <ul>
+                    <li><span> </span><a href="/accounts/">Счета</a></li>
+                    <li><span> </span><a href="/operation/">Операции</a></li>
+                    <li><span> </span><a href="/category/">Категории</a></li>
+                    <li class="last"></li>
+                </ul>
+            </li>
+            <li id="m3">
+                <a href="/budget/"></a>
+                <ul>
+                    <li><span> </span><a href="/budget/">Бюджет</a></li>
+                    <li><span> </span><a href="/targets/">Фин. цели</a></li>
+                    <li class="last"></li>
+                </ul>
+            </li>
+            <li id="m4">
+                <a href="/report/"></a>
+            </li>
+            <li id="m5">
+                <a href="/calendar/"></a>
+                <ul>
+                    <li><span> </span><a href="/calendar/#calend">Календарь</a></li>
+                    <li><span> </span><a href="/calendar/#list">События</a></li>
+                    <li class="last"></li>
+                </ul>
+            </li>
+        </ul>
+    <!--    <div class="buttons_block">
+            <a title="Добавить в календарь" id="op_addtocalendar_but">&nbsp;</a>
+        </div>-->
+    </div>
+    <!-- /mainmenu -->
+
+    <div class="ramka2">
+        <div class="cct"></div>
+        <!--меню4-->
+        <!--/меню4-->
+        <div class="mid general_cont">
+
+            <!--левая колонка-->
+            <div class="block1">
+                <ul class="control">
+                    <li id="c1"><a title="Навигация"></a></li>
+                    <li id="c2"><a title="Счета"></a></li>
+                    <li id="c3"><a title="Метки">Tags</a></li>
+                    <li id="c4"><a title="Операции"></a></li>
+                    <li id="c5"><a title="Фин. цели"></a></li>
+                </ul>
+                <div class="listing navigation c1" style="display:none">
+                    <ul>
+                    <li class="act"><span>Навигация</span>
+                        <ul>
+                            <li>
+                                <a href="/info/">Инфо-панель</a>
+                                <ul>
+                                    <li><a href="/info/">Инфо-панель</a></li>
+                                    <li><a href="/profile/">Профиль</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="/accounts/" class="parent">Счета</a>
+                                <ul>
+                                    <li><a href="/accounts/">Счета</a></li>
+                                    <li><a href="/operation/">Операции</a></li>
+                                    <li><a href="/category/">Категории</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="/budget/" class="parent">Бюджет</a>
+                                <ul>
+                                    <li><a href="/budget/">Бюджет</a></li>
+                                    <li><a href="/targets/">Фин. цели</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="/report/">Отчёты</a>
+                            </li>
+                            <li>
+                                <a href="/calendar/" class="parent">Календарь</a>
+                                <ul>
+                                    <li><a href="/calendar/#calend">Календарь</a></li>
+                                    <li><a href="/calendar/#list">Список событий</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="/logout/">Выход</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="last"><span>Прочее</span>
+                        <ul>
+                            <li><a href="/review/">Обзор</a></li>
+                            <li><a href="/feedback/">Отзывы</a></li>
+                            <li><a target="_blank" id="blog" href="http://easyfinance-ru.livejournal.com/">Блог</a></li>
+                            <li><a href="/rules/">Правила использования</a></li>
+                            <li><a href="/security/">Безопасность</a></li>
+                            <li><a href="/about/">О компании</a></li>
+                        </ul>
+                    </li>
+                    </ul>
+                </div>
+                <!--Теги-->
+                <div class="listing tags_list c3" style="display:none">
+                    <div class="title">
+                        <h2><a href="#" title="Добавить метку" class="addlink">Добавить метку</a></h2>
+                        <a href="#" title="Добавить метку" class="add">Добавить</a>
+                    </div>
+                </div>
+                <div class="edit_tag" style="display:none">
+                    <center>
+                        <div class="f_field">
+                            <label for="tag">Метка</label>
+                            <input type='text' value='' id='tag' name='tag' />
+                            <input type='hidden' value='' id='old_tag' name='old_tag' />
+                        </div>
+                    </center>
+                </div>
+                <div class="add_tag" style="display:none">
+                    <center>
+                        <div class="f_field">
+                            <label for="tags">Метка</label>
+                            <input type="text" value="" id="tag" name="tag" />
+                        </div>
+                    </center>
+                </div>
+                <!--/Теги-->
+                <!--счета-->
+                <div class="listing accounts c2"style="display:none">
+                    <div class="title">
+                        <h2><a href="#" class="addaccountlink">Добавить счёт</a></h2>
+                        <a title="Добавить" class="add">Добавить</a>
+                    </div>
+                    <dl class="bill_list">
+                        <dt class="hidden">Деньги</dt>
+                        <dd id="accountsPanelAcc0" class="hidden"> <!-- --> </dd>
+                        <dt class="hidden">Мне должны</dt>
+                        <dd id="accountsPanelAcc1" class="hidden"> <!-- --> </dd>
+                        <dt class="hidden">Я должен</dt>
+                        <dd id="accountsPanelAcc2" class="hidden"> <!-- --> </dd>
+                        <dt class="hidden">Инвестиции</dt>
+                        <dd id="accountsPanelAcc3" class="hidden"> <!-- --> </dd>
+                        <dt class="hidden">Имущество</dt>
+                        <dd id="accountsPanelAcc4" class="hidden"> <!-- --> </dd>
+                        <dt>Сумма:</dt>
+                        <dd id="accountsPanel_amount" class="amount"> <!-- --> </dd>
+                    </dl>
+                </div>
+                <!--/счета-->
+                <!--Финансовые цели-->
+                <div class="listing financobject c5"style="display:none">
+                <!-- -->
+                </div>
+                <!--/Регулярные транзакции-->
+                <div id="calendarLeft" class="listing transaction c4"style="display:none">
+                    <div class="overdue"></div>
+                    <div class="future"></div>
+                    &nbsp;&nbsp;&nbsp;&nbsp;<a href="/calendar/#list" id="AshowEvents">Журнал событий</a><br /><br />
+                </div>
+            </div>
+            <!--/левая колонка-->
+
+            <div class="block2">
+            <!--наполнение-->
+            <!--Ввод операции-->
+                {include file="operation.quick.html"}
+            <!--/Ввод операции-->
+                <?php echo $sf_content ?>
+            <!--/наполнение-->
+            </div>
+
+            <!--правая колонка-->
+            <div class="block3 ramka3">
+                <div class="ct head">
+                    <h2>Информер</h2>
+                    <ul class="action">
+                        <li class="over1" style="display: none;"><a title="настройки">настройки</a></li>
+                        <li class="over2" style="display: none;"><a title="закрыть">закрыть</a></li>
+                        <!--<li class="over3"><a href="#" title="свернуть">свернуть</a></li>-->
+                    </ul>
+                </div>
+                <!--Финсостояние-->
+                <div class="calendar_block">
+                    <h2>Фин. состояние</h2>
+                    <div class="flash informerGauge" id="divInformer0">
+                        <div id="divGaugeMain"></div>
+                    </div>
+                </div>
+                <!--/Финсостояние-->
+                <!--Курсы валют-->
+                <dl id="divExchangeRates" class="info hidden">
+                    <dt>Курсы валют</dt>
+                    <dd><div class="line"><span class="valuta">RUB</span><span class="">1</span></div><div class="line"><span class="valuta">USD</span><span class="">30.1240</span></div></dd>
+                </dl>
+                <!--/Курсы валют-->
+                <!--калькулятор-->
+                <div class="calculator_block">
+                    <h2>Калькулятор</h2>
+                    <div class="calculatorRW">
+                        <div class="input">
+                            <input type="text" value="0" id="calculatorRW"/>
+                        </div>
+                        <div class="panel">
+                            <table>
+                                <tr>
+                                    <td class="printed"><div>1</div></td>
+                                    <td class="printed"><div>2</div></td>
+                                    <td class="printed"><div>3</div></td>
+                                    <td class="printed"><div>/</div></td>
+                                    <td class="special" event="clear"><div>C</div></td>
+                                </tr>
+                                <tr>
+                                    <td class="printed"><div>4</div></td>
+                                    <td class="printed"><div>5</div></td>
+                                    <td class="printed"><div>6</div></td>
+                                    <td class="printed"><div>*</div></td>
+                                    <td class="special" event="back"><div>←</div></td>
+                                </tr>
+                                <tr>
+                                    <td class="printed"><div>7</div></td>
+                                    <td class="printed"><div>8</div></td>
+                                    <td class="printed"><div>9</div></td>
+                                    <td class="printed"><div>-</div></td>
+                                    <td rowspan="2" class="special double" event="calc"><div> = </div></td>
+                                </tr>
+                                <tr>
+                                    <td class="printed"><div>0</div></td>
+                                    <td class="printed"><div>000</div></td>
+                                    <td class="printed"><div>.</div></td>
+                                    <td class="printed"><div>+</div></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <!--/калькулятор-->
+                <!--календарь-->
+                <div class="calendar_block">
+                    <h2>Календарь</h2>
+                    <div class="calendar"></div>
+                </div>
+                <!--/календарь-->
+            </div>
+        <!--/правая колонка-->
+
+        </div>
+        <div class="ccb"></div>
+    </div>
+<!--/середина-->
 
         <!--подвал-->
         <div id="footer" style="z-index: 10; height: 0; border: 0;">
