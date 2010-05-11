18061e04563baa6b26d20a9603fd5565dcb81954
Andrey Zharikov <jet@playz.ru>
2010-05-11 18:29:59 +0400

    t1315 TKS: окно короткой анкеты

--- include/common.php
+++ include/common.php
@@ -62,6 +62,7 @@ Core::getInstance()->js = array(
 		'jquery/jquery.fancybox-1.0.0'),
     'login' => array('welcome', 'login'),
     'info' => array('info'),
+    'promo' => array('screens/promoTKS.screen'),
     'integration' => array(
         'registration',
         'login',
