b77d0b8baa87aefcfbbb0c4769ba77ddb6f4eb72
Max Kamashev <max.kamashev@gmail.ru>
2010-05-11 17:42:29 +0400

    #1311 - ТКС - короткая анкета - Миграция
     - Добавил миграцию для сохранения информации из короткой анкеты

---
+++ sf/lib/migration/doctrine/004_CreateTable_Anketa_Tks.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Downloads: Добавляем новую колонку `info` text
+ */
+class Migration004_004_CreateTable_Anketa_Tks extends Doctrine_Migration_Base
+{
+    /**
+     * Migrate
+     */
+    public  function migrate($upDown)
+    {
+        $fields = array(
+            'id' => array(
+                'type' => 'integer',
+                'unsigned' => 1,
+                'notnull' => 1,
+                'autoincrement' => true,
+                'primary' => true,
+            ),
+            'name' => array(
+                'type' => 'string',
+                'length' => 255,
+            ),
+            'surname' => array(
+                'type' => 'string',
+                'length' => 255,
+            ),
+            'patronymic' => array(
+                'type' => 'string',
+                'length' => 255,
+            ),
+            'email' => array(
+                'type' => 'string',
+                'length' => 100,
+            ),
+            'phone' => array(
+                'type' => 'string',
+                'length' => 50,
+            ),
+            'info' => array(
+                'type' => 'clob',
+                'length' => 65532,
+            ),
+        );
+        
+        $options = array(
+            'type'     => 'INNODB',
+            'charset' => 'utf8'
+        );
+
+        $this->table($upDown, 'anketa_tks', $fields, $options);
+    }
+}
