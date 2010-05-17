6d1239b018123a8d60d63dfbe40254e718a0d2e7
Svel <Svel.Sontz@gmail.com>
2010-05-17 14:53:05 +0400

    #1080 конфиг для сборки css

--- classes/Helper/Css.php
+++ classes/Helper/Css.php
@@ -2,54 +2,18 @@
 
 class Helper_Css implements _Core_Router_iHook
 {
-	public static function execRouterHook( _Core_Request $request, &$class, &$method, array &$chunks, &$templateEngine )
-	{
-		
-		if ( CSS_MINIFY )
-		{
-			$templateEngine->append('css', 'global-min.css');
-		}
-		else
-		{
-			$templateEngine->append('css', 'main.css');
-			$templateEngine->append('css', 'jquery/south-street/ui.all.css');
-			$templateEngine->append('css', 'jquery/south-street/ui.core.css');
-			$templateEngine->append('css', 'jquery/south-street/ui.resizable.css');
-			$templateEngine->append('css', 'jquery/south-street/ui.dialog.css');
-			$templateEngine->append('css', 'jquery/south-street/ui.accordion.css');
-            $templateEngine->append('css', 'jquery/south-street/ui.tabs.css');
-			$templateEngine->append('css', 'jquery/south-street/ui.datepicker.css');
-			$templateEngine->append('css', 'jquery/jquery.rw-calculator.css');
-			$templateEngine->append('css', 'jquery/jquery.jgrowl.css');
-			$templateEngine->append('css', 'jquery/fullcalendar.css');
+    public static function execRouterHook( _Core_Request $request, &$class, &$method, array &$chunks, &$templateEngine )
+    {
+        if( CSS_MINIFY ) {
+            $templateEngine->append('css', 'global-min.css');
+        } else {
+            foreach ( file( SYS_DIR_INC . 'assets/css.list') as $css )
+            {
+                $templateEngine->append('css', $css);
+            }
 
-            $templateEngine->append('css', 'dialogBox.css');
-            $templateEngine->append('css', 'menuUser.css');
-            $templateEngine->append('css', 'efTable.css');
-            $templateEngine->append('css', 'calendar.css');
-            $templateEngine->append('css', 'jquery/jHtmlArea.css');
-            $templateEngine->append('css', 'jquery/jHtmlArea.ColorPickerMenu.css');
-
-            $templateEngine->append('css', 'jquery/fancy.css');
-
-            $templateEngine->append('css', 'jquery/ufd-base.css');
-            $templateEngine->append('css', 'jquery/ufd-plain.css');
-
-            $templateEngine->append('css', 'screens/integration.screen.css');
-            $templateEngine->append('css', 'screens/promoTKS.screen.css');
-
-            $templateEngine->append('css', 'calendarOverdue.css');
-            $templateEngine->append('css', 'calendarLeft.css');
-            $templateEngine->append('css', 'report.css');
-            $templateEngine->append('css', 'expert.css');
-            $templateEngine->append('css', 'expertsList.css');
-            $templateEngine->append('css', 'profile.css');
-            $templateEngine->append('css', 'categoriesJournal.css');
-            $templateEngine->append('css', 'operationsJournal.css');
-            $templateEngine->append('css', 'budgetMaster.css');
-            $templateEngine->append('css', 'budget.css');
-            $templateEngine->append('css', 'accountsPanel.css');
-            $templateEngine->append('css', 'accountsJournal.css');
-		}
-	}
+            // не нашел эту строку в билде :-(
+            // $templateEngine->append('css', 'jquery/south-street/ui.all.css');
+        }
+    }
 }
