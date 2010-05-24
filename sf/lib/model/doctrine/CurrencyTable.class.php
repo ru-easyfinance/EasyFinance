7cd11e87134a3b8ea38da091c3ed3f4e2d337a84
Maxim Oleinik <maxim.oleinik@gmail.com>
2010-05-24 20:00:50 +0400

    t1394 Sync/Out: CurrencyTable::queryFindModifiedForSync() фильтрует по датам

--- sf/lib/model/doctrine/CurrencyTable.class.php
+++ sf/lib/model/doctrine/CurrencyTable.class.php
@@ -2,7 +2,7 @@
 
 
 /**
- * Валюта: таблица
+ * Таблица: Валюта
  */
 class CurrencyTable extends Doctrine_Table
 {
@@ -12,9 +12,18 @@ class CurrencyTable extends Doctrine_Table
      * @param  array $params
      * @return Doctrine_Query
      */
-    public function queryFindModifiedForSync(array $params)
+    public function queryFindModifiedForSync(myDatetimeRange $range, $userId)
     {
+        $dateStart = $range->getStart()->format(DATE_ISO8601);
+        $dateEnd   = $range->getEnd()->format(DATE_ISO8601);
+
         $q = $this->createQuery('c')
+            ->andWhere('c.updated_at BETWEEN CAST(? AS DATETIME) AND CAST(? AS DATETIME)', array(
+                $dateStart, $dateEnd
+            ))
+            ->orWhere('c.created_at BETWEEN CAST(? AS DATETIME) AND CAST(? AS DATETIME)', array(
+                $dateStart, $dateEnd
+            ))
             ->andWhere('c.is_active = ?', 1);
 
         return $q;
--- sf/test/unit/model/CurrencyTableTest.php
+++ sf/test/unit/model/CurrencyTableTest.php
@@ -3,10 +3,35 @@ require_once dirname(__FILE__).'/../../bootstrap/all.php';
 
 
 /**
- * Валюта: таблица
+ * Таблица: Валюта
  */
 class model_CurrencyTableTest extends myUnitTestCase
 {
+    /**
+     * Создать дату с указанным смещением в секундах от `сегодня`
+     *
+     * @param  int $shift - кол-во секунд
+     * @return DateTime
+     */
+    private function _makeDate($shift)
+    {
+        return new DateTime(date(DATE_ISO8601, time()+$shift));
+    }
+
+
+    /**
+     * Создать интервал
+     *
+     * @param  int $shiftStart - смещение для первой даты
+     * @param  int $shiftEnd   - смещение для второй даты
+     * @return myDatetimeRange
+     */
+    private function _makeDateRange($shiftStart, $shiftEnd)
+    {
+        return new myDatetimeRange($this->_makeDate($shiftStart), $this->_makeDate($shiftEnd));
+    }
+
+
     /**
      * Выбрать список измененных объектов для синка
      */
@@ -14,16 +39,40 @@ class model_CurrencyTableTest extends myUnitTestCase
     {
         $table = Doctrine::getTable('Currency');
 
-        // Деактивируем все валюты, кроме первой
-        $table->createQuery('c')
-            ->update()
-            ->set('c.is_active', 0)
-            ->where('c.id > ?', 1)
-            ->execute();
+        $c1 = $table->find(1);
+        $c1->setDateTimeObject('updated_at', $this->_makeDate(1000));
+        $c1->save();
 
-        $found = $table->queryFindModifiedForSync(array())->execute();
+        $c2 = $table->find(2);
+        $c2->setDateTimeObject('updated_at', $this->_makeDate(2000));
+        $c2->save();
+
+        $found = $table->queryFindModifiedForSync($this->_makeDateRange(1000, 1001), null)
+            ->execute();
         $this->assertEquals(1, $found->count());
-        $this->assertEquals(1, $found->getFirst()->getId());
+        $this->assertEquals($c1->getId(), $found->getFirst()->getId());
+    }
+
+
+    /**
+     * Выбрать список созданных объектов для синка
+     */
+    public function testQueryFindCreatedForSync()
+    {
+        $table = Doctrine::getTable('Currency');
+
+        $c1 = $table->find(1);
+        $c1->setDateTimeObject('created_at', $this->_makeDate(1000));
+        $c1->save();
+
+        $c2 = $table->find(2);
+        $c2->setDateTimeObject('created_at', $this->_makeDate(2000));
+        $c2->save();
+
+        $found = $table->queryFindModifiedForSync($this->_makeDateRange(1000, 1001), null)
+            ->execute();
+        $this->assertEquals(1, $found->count());
+        $this->assertEquals($c1->getId(), $found->getFirst()->getId());
     }
 
 }
