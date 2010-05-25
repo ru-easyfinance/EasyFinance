59f455ec2f048b796e0e87fc7dc558032f20fd5f
Maxim Oleinik <maxim.oleinik@gmail.com>
2010-05-25 18:44:28 +0400

    t1394 Sync/Out: даты отдаются в XML в формате ISO 8601

--- sf/apps/api/modules/sync/templates/syncOutSuccess.php
+++ sf/apps/api/modules/sync/templates/syncOutSuccess.php
@@ -6,13 +6,23 @@
  * @param array  $list    - Массив объектов
  * @param array  $columns - Список колонок, которые надо отобразить
  */
+
+$table = Doctrine::getTable($model);
 ?>
 
 <recordset type="<?php echo $model; ?>">
     <?php foreach ($list as $item): ?>
     <record id="<?php echo $item['id']; ?>">
         <?php foreach ($columns as $columnName): ?>
-        <?php echo content_tag($columnName, $item[$columnName]); ?>
+        <?php
+            $type = $table->getTypeOf($columnName);
+            if ('date' == $type || 'timestamp' == $type || 'datetime' == $type) {
+                $date = new DateTime($item[$columnName]);
+                echo content_tag($columnName, $date->format(DATE_ISO8601));
+            } else {
+                echo content_tag($columnName, $item[$columnName]);
+            }
+        ?>
         <?php endforeach; ?>
     </record>
     <?php endforeach; ?>
