bc01e9ca61d76357310604307e5fed9e04d0c422
Andrey Zharikov <jet@playz.ru>
2010-05-26 18:48:56 +0400

    t1433 календарь: ошибка при добавлении

--- www/js/models/calendar.cache.js
+++ www/js/models/calendar.cache.js
@@ -6,14 +6,14 @@ easyFinance.models.calendarCache = function(){
     var _data = {};
     var flag = 0;
     var _callback;
-    
+
     function clean(){
         // -- for IE7 --
         delete _data;
         // /-- for IE7 --
         _data = {};
     }
-    
+
     function load(startMonth, startYear, monthCount, callback){
         var realYear = startYear;
         var realMonth;
@@ -75,58 +75,64 @@ easyFinance.models.calendarCache = function(){
             }
         }
     }
-    
-    
+
+
     var _rightDate,_contentDate;
     function reloadWidgets(widget){
+
 		if (typeof(easyFinance.widgets.calendar) != 'object'){
 			widget = 'calendarPreview';
 		}
+
         if (widget == 'calendar') {
         	_rightDate = easyFinance.widgets.calendar.getCurrentDate();
         	load(_rightDate.getMonth(), _rightDate.getFullYear(), 1, function(data){
-				$('#calendar').fullCalendar('rerenderEvents');
+				$('#calendar').fullCalendar('refetchEvents');
 				easyFinance.widgets.calendarList.load(data);
         	});
-        }else if(widget == 'calendarPreview') {
+        } else if (widget == 'calendarPreview') {
 			_rightDate = easyFinance.widgets.calendarPreview.getCurrentDate();
         	load(_rightDate.getMonth(), _rightDate.getFullYear(), 1, function(data){
 				easyFinance.widgets.calendarPreview.load(data);
         	});
-		}else{
+		} else {
             _rightDate = easyFinance.widgets.calendarPreview.getCurrentDate();
             _contentDate = easyFinance.widgets.calendar.getCurrentDate();
             if (_rightDate.getFullYear() == _contentDate.getFullYear() && _rightDate.getMonth() == _contentDate.getMonth()) {
             	load(_rightDate.getMonth(), _rightDate.getFullYear(), 1, function(data){
-					$('#calendar').fullCalendar('rerenderEvents');
+					$('#calendar').fullCalendar('refetchEvents');
 					easyFinance.widgets.calendarList.load(data);
 					easyFinance.widgets.calendarPreview.load(data);
             	});
-            }else{
+            } else {
 				load(_rightDate.getMonth(), _rightDate.getFullYear(), 1, function(data){
 					easyFinance.widgets.calendarPreview.load(data);
 					load(_contentDate.getMonth(), _contentDate.getFullYear(), 1, function(data){
-						$('#calendar').fullCalendar('rerenderEvents');
+						// #1433. обновляем граф. календарь
+                        $('#calendar').fullCalendar('refetchEvents');
 						easyFinance.widgets.calendarList.load(data);
 		            });
             	});
 			}
         }
+
+        // WTF???
+        /*
         return;
-        
+
         if ($('#calend').length > 0) {
-        
+
         }
-        
+
         return;
         var tmpDate = new Date();
         if (!month || month == null) {
             month = tmpDate.getMonth();
             year = tmpDate.getFullYear();
         }
-        
+
         //		var dateInRightCalendar = $('.calendar_block .calendar').datepicker();
-        
+
         if ($('#calend').length > 0) {
             tmpDate.setMonth(month);
             month = tmpDate.getMonth();
@@ -134,22 +140,23 @@ easyFinance.models.calendarCache = function(){
             load(month, year, 3, function(data){
                 tmpDate.setMonth(month + 1);
                 $('#calendar').fullCalendar('refetchEvents');
-                
+
                 easyFinance.widgets.calendarList.load(data);
                 $('#calendar').fullCalendar('gotoDate', tmpDate.getFullYear());
-                
+
                 easyFinance.widgets.calendarPreview.load(data);
-                
+
             });
         }
         else {
-        
+
             load(month, year, 1, function(data){
                 easyFinance.widgets.calendarPreview.load(data)
             });
         }
+        */
     }
-    
+
     function init(result){
         var tmpDate = new Date();
         for (var key in result) {
@@ -175,7 +182,7 @@ easyFinance.models.calendarCache = function(){
         var retObj = $.extend({}, (_data[leftKey] || {}), (_data[generalKey] || {}), (_data[rightKey] || {}));
         return retObj;
     }
-    
+
     function removeChain(chainId){
         for (var mainKey in _data) {
             for (var key in _data[mainKey]) {
@@ -185,7 +192,7 @@ easyFinance.models.calendarCache = function(){
             }
         }
     }
-    
+
     function removeElements(elementsArray){
         if (typeof(elementsArray) == "number") {
             elementsArray = [elementsArray];
@@ -200,7 +207,7 @@ easyFinance.models.calendarCache = function(){
             }
         }
     }
-    
+
     function acceptElements(elementsArray){
         if (typeof(elementsArray) == "number") {
             elementsArray = [elementsArray];
@@ -215,7 +222,7 @@ easyFinance.models.calendarCache = function(){
             }
         }
     }
-    
+
     return {
         init: init,
         load: load,
@@ -226,5 +233,5 @@ easyFinance.models.calendarCache = function(){
         removeElements: removeElements,
         acceptElements: acceptElements
     }
-    
+
 }();
