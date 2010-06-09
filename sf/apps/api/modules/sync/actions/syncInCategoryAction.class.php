88c734b33a4eaaa4b17f719980494bab4bb8c3e5
Svel <Svel.Sontz@gmail.com>
2010-06-09 18:46:00 +0400

    t1460 Sync/In категории: набросок

---
+++ sf/apps/api/modules/sync/actions/syncInCategoryAction.class.php
@@ -0,0 +1,16 @@
+<?php
+require_once(dirname(__FILE__).'/../lib/myBaseSyncInAction.php');
+
+/**
+ * Sync: получить набор объектов-категорий
+ */
+class syncInCategoryAction extends myBaseSyncInAction
+{
+    /**
+     * Execute
+     */
+    public function execute($request)
+    {
+    }
+
+}
---
+++ sf/test/functional/api/sync/InCategoryTest.php
@@ -0,0 +1,11 @@
+<?php
+require_once dirname(__FILE__).'/../../../bootstrap/all.php';
+
+
+/**
+ * Синхронизация: получить список категорий
+ */
+class api_sync_InCategoryTest extends mySyncInFunctionalTestCase
+{
+    protected $app = 'api';
+}
