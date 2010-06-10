eb4acda1c2915a3346267fb38390da663f9926ab
Svel <Svel.Sontz@gmail.com>
2010-06-10 21:14:08 +0400

    t1460 Sync/In категории: получение xml * draft
      * функциональные тесты
      * получение xml

--- sf/apps/api/modules/sync/actions/syncInCategoryAction.class.php
+++ sf/apps/api/modules/sync/actions/syncInCategoryAction.class.php
@@ -11,6 +11,52 @@ class syncInCategoryAction extends myBaseSyncInAction
      */
     public function execute($request)
     {
+        // $userId = $this->getUser()->getId();
+        if (null === ($userId = $request->getParameter('user_id'))) {
+            $this->getResponse()->setHttpHeader('WWW_Authenticate', "Authentification required");
+            return $this->raiseError("Authentification required", 0, 401);
+        }
+
+        if (0 === strlen($rawXml = $request->getContent())) {
+            return $this->raiseError("Expected XML data");
+        }
+
+        $xml = simplexml_load_string($rawXml);
+
+        $count = (int) count($xml->recordset[0]);
+        $limit = sfConfig::get('app_records_sync_limit', 100);
+
+        if ($count <= 0) {
+            return $this->raiseError("Expected at least one record");
+        } elseif ($count > $limit) {
+            return $this->raiseError("More than 'limit' ({$limit}) objects sent, {$count}");
+        }
+
+        $results = array();
+        // foreach
+
+        $this->setVar('results', $results, $noEscape = false);
+
+        return sfView::SUCCESS;
+    }
+
+
+    /**
+     * Делает из объекта SimpleXML массив
+     *
+     * @see    myBaseSyncInAction
+     * @param  SimpleXMLElement $record
+     * @return array
+     */
+    protected function prepareArray(SimpleXMLElement $record)
+    {
+        $data = array();
+        $data['id']          = (string) $record['id'];
+        $data['cid']         = (string) $record['cid'];
+        $data['created_at']  = (string) $record->created_at;
+        $data['updated_at']  = (string) $record->updated_at;
+        $data['deleted_at']  = ((string) $record['deleted']) ? (string) $record->updated_at : null;
+        return $data;
     }
 
 }
--- sf/test/functional/api/sync/InCategoryTest.php
+++ sf/test/functional/api/sync/InCategoryTest.php
@@ -8,4 +8,122 @@ require_once dirname(__FILE__).'/../../../bootstrap/all.php';
 class api_sync_InCategoryTest extends mySyncInFunctionalTestCase
 {
     protected $app = 'api';
+
+
+    /**
+     * Возвращает стандартный валидный набор полей и значений объекта
+     *
+     * @return array
+     */
+    protected function getDefaultModelData()
+    {
+        return array(
+            'id'          => null,
+            'created_at'  => $this->_makeDate(-1000),
+            'updated_at'  => $this->_makeDate(0),
+            'deleted_at'  => null,
+        );
+    }
+
+
+    /**
+     * Вернуть название модели
+     *
+     * @return string
+     */
+    protected function getModelName()
+    {
+        return 'category';
+    }
+
+
+    /**
+     * Отправляет XML
+     *
+     * @param  string $xml      XML-строка
+     * @param  int    $code     Код ответа сервера
+     * @return sfTestFunctional Возвращает браузер @see sfTestBrowser
+     */
+    protected function myXMLPost($xml = null, $code = 200)
+    {
+        return $this
+            ->postAndCheckXML("sync", "syncInCategory", $xml, "sync_in_category", $code);
+    }
+
+
+    /**
+     * Ошибка при отправке без авторизации
+     * (пока без ID пользователя в query string)
+     */
+    public function testPostCategoryAuthError()
+    {
+        $this->_user->setId(null);
+
+        $this->checkSyncInError(null, 401, 'Authentification required');
+    }
+
+
+    /**
+     * Ошибка при отправке пустого POST
+     * нет данных на входе
+     */
+    public function testPostCategoryEmptyPostError()
+    {
+        $this->checkSyncInError(null, 400, 'Expected XML data');
+    }
+
+
+    /**
+     * Пустой xml, отсутствуют записи для обработки
+     */
+    public function testPostCategoryEmptyXMLError()
+    {
+        $this->checkSyncInError($this->getXMLHelper()->getEmptyRequest(), 400, 'Expected at least one record');
+    }
+
+
+    /**
+     * Входящий xml содержит слишком много записей
+     */
+    public function testPostCategoryRecordsLimitError()
+    {
+        $this->browser->getContext(true);
+
+        $this->assertNotNull(sfConfig::get('app_records_sync_limit'));
+        $this->browser->setConfigValue('app_records_sync_limit', $max = 2);
+
+        $xml = $this->getXMLHelper()->makeCollection($max+1);
+
+        $this->checkSyncInError($xml, 400, "More than 'limit' ({$max}) objects sent, " . $max + 1);
+    }
+
+
+    /**
+     * Принять валидный xml
+     */
+    public function testPostCategorySingle()
+    {
+        $expectedData = array(
+            'created_at'  => $this->_makeDate(-10000),
+            'updated_at'  => $this->_makeDate(-300),
+            'deleted_at'  => null,
+            'cid'         => 5,
+        );
+
+        $xml = $this->getXMLHelper()->make($expectedData);
+
+        $this
+            ->myXMLPost($xml, 200)
+            ->with('response')->begin()
+                ->checkElement('resultset', 1)
+                ->checkElement('resultset record', 1)
+                ->checkElement('resultset[type="account"] record[id][success="true"]', 'OK')
+                ->checkElement(sprintf('resultset record[cid="%d"]', $expectedData['cid']), 'OK')
+            ->end();
+
+        unset($expectedData['cid']); // у записи нет такого поля
+        $this->browser
+            ->with('model')->check('Category', $expectedData, 1);
+    }
+
 }
