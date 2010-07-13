665e88b99ca0dda8ed7c586ab0576a1e67bbb0d0
Svel <Svel.Sontz@gmail.com>
2010-07-13 15:24:49 +0400

    t1489 citibank: почистил myParseEmailImport после применения патча
      * заменил табы на пробелы
      * поправил форматирование

--- sf/lib/mail/myParseEmailImport.php
+++ sf/lib/mail/myParseEmailImport.php
@@ -25,14 +25,15 @@ class myParseEmailImport
      */
     private $_to;
 
+
     /**
      * Инициализация
      *
-     * @param string $mail текст письма
+     * @param string      $mail   текст письма
      * @param EmailParser $parser парсер
-     * @param string $to email получателя
+     * @param string      $to     email получателя
      */
-    public function __construct ( $mail, EmailParser $parser, $to )
+    public function __construct ($mail, EmailParser $parser, $to)
     {
         $this->_parser = $parser;
         $this->_mail = $mail;
@@ -43,40 +44,43 @@ class myParseEmailImport
     /**
      * Распарсить email и получить массив данных
      *
-     * @param mixed $forced_operation_id если установлен - id операции устанавливаемый в ручную
-     * @return array | int в случае ошибки
+     * @param  mixed     $forced_operation_id если установлен - id операции устанавливаемый в ручную
+     * @return array|int в случае ошибки
      */
-    public function getData( $forced_operation_id=false )
+    public function getData($forced_operation_id = false)
     {
         // Последние 4 цифры карты
-        preg_match( '/' . $this->_parser->getAccountRegexp() . '/im', $this->_mail, $matches );
-        $lastFourDigits = ( isset( $matches[1] ) ) ? $matches[1] : false;
+        preg_match('/' . $this->_parser->getAccountRegexp() . '/im', $this->_mail, $matches);
+        $lastFourDigits = (isset($matches[1])) ? $matches[1] : false;
 
         // Сумма платежа
-        preg_match( '/' . $this->_parser->getTotalRegexp() . '/im', $this->_mail, $matches );
-        $amount = ( isset( $matches[1] ) ) ? $matches[1] : false;
+        preg_match('/' . $this->_parser->getTotalRegexp() . '/im', $this->_mail, $matches);
+        $amount = (isset($matches[1])) ? $matches[1] : false;
 
         // Вычищаем левые символы в сумме
-        $amount = preg_replace( "/[^\d\.]+/", "", $amount);
+        $amount = preg_replace("/[^\d\.]+/", "", $amount);
 
         // Детали операции
-        preg_match( '/' . $this->_parser->getDescriptionRegexp() . '/im', $this->_mail, $matches );
+        preg_match('/' . $this->_parser->getDescriptionRegexp() . '/im', $this->_mail, $matches);
 
-        if ( isset( $matches[0] ) ) unset( $matches[0] );
-        $description = ( count( $matches ) ) ? implode( " ", $matches ) : "";
+        if (isset($matches[0])) {
+            unset($matches[0]);
+        }
+
+        $description = (count($matches)) ? implode(" ", $matches) : "";
 
         // Направление движения средств (тип)
         $type = $this->_parser->getType() ? Operation::TYPE_PROFIT : Operation::TYPE_EXPENSE;
 
         $operationData = array(
-            'email'         => $this->_to,
-            'amount'        => (string)$amount,
-            'description'   => trim( $description ),
-            'type'          => (string)$type,
-            'account'       => (string)$lastFourDigits,
+            'email'       => $this->_to,
+            'amount'      => (string) $amount,
+            'description' => trim($description),
+            'type'        => (string) $type,
+            'account'     => (string) $lastFourDigits,
             // Источник, что-то наподобие citi1234 для привязки источника и 4-х последних знаков карты к счету
-            'source'        => substr( $this->_parser->getEmailSource()->getName(), 0, 4) . $lastFourDigits,
-            'id'            => ( $forced_operation_id ) ? $forced_operation_id : uniqid()
+            'source'      => substr($this->_parser->getEmailSource()->getName(), 0, 4) . $lastFourDigits,
+            'id'          => ($forced_operation_id) ? $forced_operation_id : uniqid(),
         );
 
         return $operationData;
@@ -86,10 +90,10 @@ class myParseEmailImport
     /**
      * Статический метод производящий декодировку письма
      *
-     * @param string $input полный оригинальный текст email с заголовками
-     * @return array либо false, в случае неверного формата входящих данных (отсутствует заголовок или часть заголовка)
+     * @param  string $input полный оригинальный текст email с заголовками
+     * @return array|false, в случае неверного формата входящих данных (отсутствует заголовок или часть заголовка)
      */
-    public static function getEmailData( $input )
+    public static function getEmailData($input)
     {
         $zPath = sfConfig::get('sf_root_dir') . '/lib/vendor';
         set_include_path($zPath . PATH_SEPARATOR . get_include_path());
@@ -97,38 +101,37 @@ class myParseEmailImport
         $message = new Zend_Mail_Message(array('raw' => $input));
 
         $headers = $message->getHeaders();
-        if ( !count( $headers ) ) return false;
-        if ( !isset( $headers['subject'] ) ||
-             !isset( $headers['from'] ) ||
-             !isset( $headers['to'] )
-        ) return false;
+
+        if (!count($headers)) {
+            return false;
+        }
+
+        if (!isset($headers['subject']) || !isset($headers['from']) || !isset($headers['to'])) {
+            return false;
+        }
 
         // Трансформация сабжекта в формате ?UTF-8?XXX
         $subject = iconv_mime_decode($headers['subject'], 0, 'UTF-8');
 
-        if ( isset( $headers['content-transfer-encoding'] ) && ( $headers['content-transfer-encoding'] == "quoted-printable" ) )
-        {
-        	// Декодирование quoted printable
-            $body = quoted_printable_decode( $message->getContent() );
-        }
-    	elseif ( isset( $headers['content-transfer-encoding'] ) && ( $headers['content-transfer-encoding'] == "base64" ) )
-        {
-        	// Декодирование тела письма в base64
-            $body = base64_decode( $message->getContent() );
-        }
-        else
-        {
-            $body = $message->getContent();
+        $body = $message->getContent();
+        if (isset($headers['content-transfer-encoding']) && ($headers['content-transfer-encoding'] == "quoted-printable")) {
+            // Декодирование quoted printable
+            $body = quoted_printable_decode($body);
+        } elseif (isset($headers['content-transfer-encoding']) && ($headers['content-transfer-encoding'] == "base64")) {
+            // Декодирование тела письма в base64
+            $body = base64_decode($body);
         }
 
         $data = array(
-            'from' => self::_cleanEmail( $headers['from'] ),
-            'to' => self::_cleanEmail( $headers['to'] ),
+            'from'    => self::_cleanEmail($headers['from']),
+            'to'      => self::_cleanEmail($headers['to']),
             'subject' => $subject,
-            'body' => $body
+            'body'    => $body,
         );
+
         return $data;
     }
+
 
     /**
      * Email-ы могут вернуться как в виде "name@domain.zone", так и в виде "Имя пользователя <name@domain.zone>"
@@ -137,17 +140,16 @@ class myParseEmailImport
      * @param string $email
      * @return string
      */
-    private static function _cleanEmail( $email ) {
+    private static function _cleanEmail($email) {
+        $matches = array();
 
-    	$matches = array();
+        if (preg_match("/<(.*)>/i", $email, $matches)) {
+            if (isset( $matches[1])) {
+                $email = $matches[1];
+            }
+        }
 
-    	if ( preg_match("/<(.*)>/i", $email, $matches ) ) {
-    		if ( isset( $matches[1]) ) {
-    			$email = $matches[1];
-    		}
-    	}
-
-    	return $email;
+        return $email;
     }
 
 }
