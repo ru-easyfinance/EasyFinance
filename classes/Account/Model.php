c7d2b731589fc6741fcf2eb1ecd6a2ec8d9eb437
Anton Belousov <anton.n.belousov@gmail.com>
2010-07-28 16:11:03 +0400

    t1679 fixed account balances when having transfers

--- classes/Account/Model.php
+++ classes/Account/Model.php
@@ -180,7 +180,7 @@ class Account_Model
     private function _getTotalByAccounts(array $accountIds)
     {
         $sql = "SELECT
-                    o.account_id AS account_id,
+                    acc.account_id AS account_id,
                     SUM(CASE 
                         	WHEN o.account_id = acc.account_id THEN o.money
                         	WHEN IFNULL(o.transfer_amount, 0) = 0 THEN ABS(o.money)
