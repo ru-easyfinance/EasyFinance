bb54b9a8c3cf8c1901e4ed62a58af1baa019f5af
Anton Minin <anton.a.minin@gmail.com>
2010-08-20 15:58:33 +0400

    Исправил старый баг с DateTime, несовместимый с PHP 5.2

--- sf/apps/frontend/modules/res/actions/calendarComponent.class.php
+++ sf/apps/frontend/modules/res/actions/calendarComponent.class.php
@@ -12,21 +12,32 @@ class calendarComponent extends sfComponent
     public function execute($request)
     {
         $user = $this->getUser()->getUserRecord();
-        // Если интервал не задан, будем выводить операции с начала месяца до сегодня
+        // Если интервал не задан,
+        // будем выводить операции с начала месяца до сегодня
         // TODO: уточнить эти умолчания
-        if(empty($this->dateStart)) {
+        if (empty($this->dateStart)) {
             $this->dateStart = date('Y-m-', time()) . '01';
         }
-        if(empty($this->dateEnd)) {
+        if (empty($this->dateEnd)) {
             $this->dateEnd = date('Y-m-d', time());
         }
-        $calendarOperations = Doctrine::getTable('Operation')->queryFindWithCalendarChainsForPeriod(
-              $user
-            , DateTime::createFromFormat('Y-m-d', $this->dateStart)
-            , DateTime::createFromFormat('Y-m-d', $this->dateEnd)
+
+        list($y, $m, $d) = explode('-', $this->dateStart);
+        $dateStart = new DateTime();
+        $dateStart->setDate($y, $m, $d);
+
+        list($y, $m, $d) = explode('-', $this->dateEnd);
+        $dateEnd = new DateTime();
+        $dateEnd->setDate($y, $m, $d);
+
+        $calendarOperations = Doctrine::getTable('Operation')
+            ->queryFindWithCalendarChainsForPeriod(
+                $user, $dateStart, $dateEnd
             )->fetchArray();
 
-        $this->setVar('calendarOperations', $calendarOperations, $noEscape = true);
+        $this->setVar(
+            'calendarOperations', $calendarOperations, $noEscape = true
+        );
     }
 
 }
