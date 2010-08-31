d55dc6c651d2cb44c1ec7b68b2705334f086f984
Anton Minin <anton.a.minin@gmail.com>
2010-08-31 12:25:13 +0400

    Залил правки Игнатова

--- sf/apps/frontend/templates/_common/mainmenu.php
+++ sf/apps/frontend/templates/_common/mainmenu.php
@@ -4,8 +4,6 @@
             <a href="/info/"></a>
             <ul>
                 <li><span> </span><a href="/info/">Инфо-панель</a></li>
-                <li><span> </span><a href="/profile/">Профиль</a></li>
-                <li><span> </span><a href="/my/services/">Услуги</a></li>
                 <li><span> </span><a href="https://<?php echo URL_ROOT_MAIN; ?>integration/">EasyBank</a></li>
                 <li class="last"></li>
             </ul>
@@ -27,9 +25,6 @@
                 <li class="last"></li>
             </ul>
         </li>
-        <li id="m4">
-            <a href="/report/"></a>
-        </li>
         <li id="m5">
             <a href="/calendar/"></a>
             <ul>
@@ -38,16 +33,42 @@
                 <li class="last"></li>
             </ul>
         </li>
+        <li id="m4">
+            <a href="/report/"></a>
+        </li>
         <li id="m6">
             <a href="/integration/"></a>
         </li>
-        <li id="m7">
-            <a href="#"></a>
+        <li id="m8">
+            <a href="http://wiki.easyfinance.ru/tiki-view_blog.php?blogId=1"></a>
             <ul>
+                <li><span> </span><a href="http://wiki.easyfinance.ru/tiki-view_blog.php?blogId=1">Блог</a></li>
+                <li><span> </span><a href="http://wiki.easyfinance.ru/tiki-view_articles.php">Статьи</a></li>
+                <li><span> </span><a href="http://wiki.easyfinance.ru/tiki-view_blog.php?blogId=2">Отзывы</a></li>
+                <li><span> </span><a href="http://wiki.easyfinance.ru/tiki-view_blog.php?blogId=4">Сми о нас</a></li>
+                <li class="last"></li>
+            </ul>
+        </li>
+        <li id="m7" onMouseOver="$('#m9').hide(); $('#m7>a').text('');" onMouseOut="$('#m9').show(); $('#m7>a').text('Помощь');">
+            <a href="#">Помощь</a>
+            <ul>
+                <li class="first"></li>
                 <li><span> </span><a id="linkMainMenuFeedback" href="#">Обратная связь</a></li>
                 <li><span> </span><a id="linkMainMenuVideoGuide" href="#">Видеогид</a></li>
+                <li><span> </span><a href="http://wiki.easyfinance.ru/tiki-read_article.php?articleId=14">Частые вопросы</a></li>
+                <li><span> </span><a href="/review/">Обзор</a></li>
+                <li class="last"></li>
+            </ul>
+        </li>
+        <li id="m9" onMouseOver="$('#m7').hide(); $('#m9>a').text('');" onMouseOut="$('#m7').show(); $('#m9>a').text('Настройки');">
+            <a href="/profile/">Настройки</a>
+            <ul>
+                <li class="first"></li>
+                <li><span> </span><a href="/profile/">Профиль</a></li>
+                <li><span> </span><a href="/my/services/">Услуги</a></li>
                 <li class="last"></li>
             </ul>
         </li>
     </ul>
 </div>
+
