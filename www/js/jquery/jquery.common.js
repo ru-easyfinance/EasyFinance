092867364744b9d05ac81b3bd8da325e5ceab8b1
Tyryshkin Stanislav <tyryshkin1988@gmail.com>
2010-09-08 14:05:56 +0700

    Добавлен common.js, с полезными рутинными функциями: - outerHTML - getTagName

---
+++ www/js/jquery/jquery.common.js
@@ -0,0 +1,22 @@
+(function($) {
+    // Получить имя Тэга для текущего нода
+    $.fn.tagName = function() {
+        var node = this.get(0);
+        if(!node) {
+            return '';
+        }
+        return this.get(0).tagName.toLowerCase();
+    }
+    // Перечисление тэгов
+    $.tagName = 
+    {
+        'undefined': '',
+        'input': 'input',
+        'button': 'button',
+        // и.тд.
+    };
+    // Получить HTML текушего нода
+    $.fn.outerHTML = function(s) {
+        return (s) ? this.before(s).remove() : $("<p>").append(this.eq(0).clone()).html();
+    }
+})(jQuery);
