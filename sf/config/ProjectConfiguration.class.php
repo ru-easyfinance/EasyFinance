ad8d85d5b65569760e43cd64b63efd2ddc7247bc
Svel <Svel.Sontz@gmail.com>
2010-09-14 15:35:23 +0400

    t1997 [logger] подключил логгер
      * подключил в конфигурации
      * без подключения в PDA падает на тестах
      * пример вызова:
    
    $this->dispatcher->notify(
        new sfEvent('SUBJ', 'app.activity', array(
            // обязательное
            'name' => 'название события',
            'description' => 'описание события',
            // и по усмотрению, по ситуации
            'state' => 'info', // состояние: fail, info, notice, warning
            'object' => 1, // идентификатор объекта
            'component' => 'stringName', // компонент приложения текстом
            'env' => 'TEXT', // данные события
            'user' => object|id, // идентификатор пользователя
        ))
    );
    
    SUBJ пока не используется, практика покажет что логичнее туда передавать

--- sf/config/ProjectConfiguration.class.php
+++ sf/config/ProjectConfiguration.class.php
@@ -43,7 +43,8 @@ class ProjectConfiguration extends sfProjectConfiguration
             'sfDoctrinePlugin',
             'sfPhpunitPlugin',
             'myAuthPlugin',
-            'sfWebBrowserPlugin'
+            'sfWebBrowserPlugin',
+            'myDoctrineLoggerPlugin',
         ));
     }
 
