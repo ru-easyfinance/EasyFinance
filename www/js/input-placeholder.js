7bbfdf226a82ae85e870d2f75aaa4e1701c64d5e
ant_Ti <abietis@gmail.com>
2010-09-23 05:17:42 +0400

    t2036 Новая форма логина на главной странице
    Сверстана новая форма логина
    Исправлен файл для правильной работы с автоподстановкой паролей и работы с браузерами на Webkit
    Нарисованы фоновая картинка для выпадающего меню и картинка логина
    Исправлена фоновая картинка шапки

--- www/js/input-placeholder.js
+++ www/js/input-placeholder.js
@@ -13,17 +13,21 @@ _InputPlaceholder = {
             var el = $(this),
                 input = el.next();
             if(el.text() != '') {
-                input.data('value', input.val());
-                el.next().unbind('focus.placeholder').unbind('blur.placeholder').bind('focus.placeholder', function() {
-                    self.show(el, $(this));
-                }).bind('blur.placeholder', function() {
-                    if(el.hasClass('error')) {
-                        if(input.val() == input.data('value')) self.hide(el, $(this));
-                    } else {
-                        if(input.val() == '') self.hide(el, $(this));
-                    }
-                });
-                if(el.hasClass('error') || el.next().val() == '') self.hide(el, el.next());
+                if($.browser.webkit || $.browser.safari) {
+                    input.attr('placeholder', el.text());
+                } else {
+                    input.data('value', input.val());
+                    el.next().unbind('focus.placeholder').unbind('blur.placeholder').bind('focus.placeholder', function() {
+                        self.show(el, $(this));
+                    }).bind('blur.placeholder', function() {
+                        if(el.hasClass('error')) {
+                            if(input.val() == input.data('value')) self.hide(el, $(this));
+                        } else {
+                            if(input.val() == '') self.hide(el, $(this));
+                        }
+                    });
+                    if(el.hasClass('error') || el.next().val() == '') self.hide(el, el.next());
+                }
             }
         });
     },
