f5970778bfabcc2f406594d50e39985ebc5525d7
Konstantin Kitmanov <doctor.hogart@gmail.com>
2010-10-14 15:37:37 +0400

    t2068 Заглушка страницы "обзора"
    Переверстал
    переписал скрипт

--- sf/apps/frontend/modules/page/templates/_review.php
+++ sf/apps/frontend/modules/page/templates/_review.php
@@ -1,81 +1,74 @@
-<div class="map" >
-    <div class="registration" title="Нажмите для увеличения">
-        <a id="aRegistration" class="fancy" href="/img/i/review/registrationbig.png" title="Регистрация"></a>
-    <!--
-    <div class="popup_map2"><div style="overflow: hidden; display: none; position: absolute; z-index: 1002; outline-color: -moz-use-text-color; outline-style: none; outline-width: 0px; height: auto; top: -180px; left: -130px;" class="w_dialog mapContent"><div class="link close"></div><div class="w_d_head"></div><div class="w_d_body">
-    <img src="/img/i/review/registrationbig.png" /><p></p></div><div class="w_d_foot"></div></div></div>
-    -->
+<div class="b-review js-widget js-widget-review">
+    <h1 class="b-review-title">Эта страница всегда доступна из пункта меню &laquo;<i class="b-review-help"></i> ➝ Обзор&raquo;</h1>
+    <div class="w-review-picture">
+        <div class="b-review-clip b-review-clip__1" title="Обзор системы, настройка валют и смс">
+            <div class="b-review-clip-point"></div>
+            <div class="b-review-clip-preview">
+                <img class="b-clip-thumb" src="/img/i/review/review-preview.png" alt=""/>
+                <i class="b-clip-play"></i>
+            </div>
+        </div>
+        <div class="b-review-clip b-review-clip__2" title="Настройка счетов и категорий">
+            <div class="b-review-clip-point"></div>
+            <div class="b-review-clip-preview">
+                <img class="b-clip-thumb" src="/img/i/review/review-preview.png" alt=""/>
+                <i class="b-clip-play"></i>
+            </div>
+        </div>
+        <div class="b-review-clip b-review-clip__3" title="Вносим доходы и расходы">
+            <div class="b-review-clip-point"></div>
+            <div class="b-review-clip-preview">
+                <img class="b-clip-thumb" src="/img/i/review/review-preview.png" alt=""/>
+                <i class="b-clip-play"></i>
+            </div>
+        </div>
+        <div class="b-review-clip b-review-clip__4" title="Учитываем кредиты, долги, депозиты">
+            <div class="b-review-clip-point"></div>
+            <div class="b-review-clip-preview">
+                <img class="b-clip-thumb" src="/img/i/review/review-preview.png" alt=""/>
+                <i class="b-clip-play"></i>
+            </div>
+        </div>
+        <div class="b-review-clip b-review-clip__5" title="Формируем бюджет">
+            <div class="b-review-clip-point"></div>
+            <div class="b-review-clip-preview">
+                <img class="b-clip-thumb" src="/img/i/review/review-preview.png" alt=""/>
+                <i class="b-clip-play"></i>
+            </div>
+        </div>
+        <div class="b-review-clip b-review-clip__6" title="Заполняем календарь будущих платежей">
+            <div class="b-review-clip-point"></div>
+            <div class="b-review-clip-preview">
+                <img class="b-clip-thumb" src="/img/i/review/review-preview.png" alt=""/>
+                <i class="b-clip-play"></i>
+            </div>
+        </div>
+        <div class="b-review-clip b-review-clip__7" title="Выбираем финансовую цель">
+            <div class="b-review-clip-point"></div>
+            <div class="b-review-clip-preview">
+                <img class="b-clip-thumb" src="/img/i/review/review-preview.png" alt=""/>
+                <i class="b-clip-play"></i>
+            </div>
+        </div>
+        <div class="b-review-clip b-review-clip__8" title="Следим за финансовым состоянием">
+            <div class="b-review-clip-point"></div>
+            <div class="b-review-clip-preview">
+                <img class="b-clip-thumb" src="/img/i/review/review-preview.png" alt=""/>
+                <i class="b-clip-play"></i>
+            </div>
+        </div>
+        <div class="b-review-clip b-review-clip__9" title="Анализируем отчеты">
+            <div class="b-review-clip-point"></div>
+            <div class="b-review-clip-preview">
+                <img class="b-clip-thumb" src="/img/i/review/review-preview.png" alt="Анализируем отчеты"/>
+                <i class="b-clip-play"></i>
+            </div>
+        </div>
     </div>
 
-    <div class="CreateSales" title="Нажмите для увеличения">
-        <a id="aCreateSales" class="fancy" href="/img/i/review/createsalesbig.png" title="Счета"></a>
-    <!--
-    <div class="popup_map2"><div style="overflow: hidden; display: none; position: absolute; z-index: 1002; outline-color: -moz-use-text-color; outline-style: none; outline-width: 0px; height: auto; top: -180px; left: -130px;" class="w_dialog mapContent2"><div class="link close"></div><div class="w_d_head"></div><div class="w_d_body">
-    <img src="/img/i/review/createsalesbig.png" /><p></p></div><div class="w_d_foot"></div></div></div>
-    -->
-    </div>
-
-    <div class="categoryEdit" title="Нажмите для увеличения">
-        <a id="aCategoryEdit" class="fancy" href="/img/i/review/categoryeditbig.png" title="Категории"></a>
-    <!--
-    <div class="popup_map2"><div style="overflow: hidden; display: none; position: absolute; z-index: 1002; outline-color: -moz-use-text-color; outline-style: none; outline-width: 0px; height: auto; top: -180px; left: 15px;" class="w_dialog mapContent3"><div class="link close"></div><div class="w_d_head"></div><div class="w_d_body">
-    <img src="/img/i/review/categoryeditbig.png" /><p></p></div><div class="w_d_foot"></div></div></div>
-    -->
-    </div>
-
-    <div class="closeOperation" title="Нажмите для увеличения">
-        <a id="aCloseOperation" class="fancy" href="/img/i/review/closeoperationbig.png" title="Операции"></a>
-    <!--
-    <div class="popup_map2"><div style="overflow: hidden; display: none; position: absolute; z-index: 1002; outline-color: -moz-use-text-color; outline-style: none; outline-width: 0px; height: auto; top: -10px; left: -10px;" class="w_dialog mapContent4"><div class="link close"></div><div class="w_d_head"></div><div class="w_d_body">
-    <img src="/img/i/review/closeoperationbig.png" /><p></p></div><div class="w_d_foot"></div></div></div>
-    -->
-    </div>
-
-    <div class="infoPanel" title="Нажмите для увеличения">
-        <a id="aInfoPanel" class="fancy" href="/img/i/review/infopanelbig.png" title="Инфопанель"></a>
-    <!--
-    <div class="popup_map2"><div style="overflow: hidden; display: none; position: absolute; z-index: 1002; outline-color: -moz-use-text-color; outline-style: none; outline-width: 0px; height: auto; top: -10px; left: -10px;" class="w_dialog mapContent5"><div class="link close"></div><div class="w_d_head"></div><div class="w_d_body">
-    <img src="/img/i/review/infopanelbig.png" /><p></p></div><div class="w_d_foot"></div></div></div>
-    -->
-    </div>
-
-    <div class="createTransaction" title="Нажмите для увеличения">
-        <a id="aCreateTransaction" class="fancy" href="/img/i/review/createtransactionbig.png" title="Регулярные операции"></a>
-    <!--
-    <div class="popup_map2"><div style="overflow: hidden; display: none; position: absolute; z-index: 1002; outline-color: -moz-use-text-color; outline-style: none; outline-width: 0px; height: auto; top: -10px; left: -10px;" class="w_dialog mapContent6"><div class="link close"></div><div class="w_d_head"></div><div class="w_d_body">
-    <img src="/img/i/review/createtransactionbig.png" /><p></p></div><div class="w_d_foot"></div></div></div>
-    -->
-    </div>
-
-    <div class="expertView" title="Нажмите для увеличения">
-        <a id="aExpertView" class="fancy" href="/img/i/review/reportcirclebig.png" title="Отчёты"></a>
-    <!--
-    <div class="popup_map2"><div style="overflow: hidden; display: none; position: absolute; z-index: 1002; outline-color: -moz-use-text-color; outline-style: none; outline-width: 0px; height: auto; top: -10px; left: -270px;" class="w_dialog mapContent7"><div class="link close"></div><div class="w_d_head"></div><div class="w_d_body">
-    <img src="/img/i/review/expertviewbig.png" /><p></p></div><div class="w_d_foot"></div></div></div>
-    -->
-    </div>
-
-    <div class="analise" title="Нажмите для увеличения">
-        <a id="aAnalise" class="fancy" href="/img/i/review/analisebig.png" title="Анализ"></a>
-    <!--
-    <div class="popup_map2"><div style="overflow: hidden; display: none; position: absolute; z-index: 1002; outline-color: -moz-use-text-color; outline-style: none; outline-width: 0px; height: auto; top: -10px; left: -70px;" class="w_dialog mapContent8"><div class="link close"></div><div class="w_d_head"></div><div class="w_d_body">
-    <img src="/img/i/review/analisebig.png" /><p></p></div><div class="w_d_foot"></div></div></div>
-    -->
-    </div>
-
-    <div class="financeTarget" title="Нажмите для увеличения">
-        <a id="aFinanceTarget" class="fancy" href="/img/i/review/financetargetbig.png" title="Цели"></a>
-    <!--
-    <div class="popup_map2"><div style="overflow: hidden; display: none; position: absolute; z-index: 1002; outline-color: -moz-use-text-color; outline-style: none; outline-width: 0px; height: auto; top: -10px; left: -10px;" class="w_dialog mapContent9"><div class="link close"></div><div class="w_d_head"></div><div class="w_d_body">
-    <img src="/img/i/review/financetargetbig.png" /><p></p></div><div class="w_d_foot"></div></div></div>
-    -->
-    </div>
-
-    <div class="capitalCreate" title="Нажмите для увеличения">
-        <a id="aCapitalCreate" class="fancy" href="/img/i/review/capitalcreatebig.png" title="Бюджет"></a>
-    <!--
-    <div class="popup_map2"><div style="overflow: hidden; display: none; position: absolute; z-index: 1002; outline-color: -moz-use-text-color; outline-style: none; outline-width: 0px; height: auto; top: -10px; left: -30px;" class="w_dialog mapContent10"><div class="link close"></div><div class="w_d_head"></div><div class="w_d_body">
-    <img src="/img/i/review/capitalcreatebig.png" /><p></p></div><div class="w_d_foot"></div></div></div>
-    -->
+    <div class="b-player js-review-dialog" style="display: none;">
+        <div class="l-dialog-content">
+            <a class="b-player-container js-review-playercontainer" id="reviewPlayer"></a>
+        </div>
     </div>
 </div>
--- www/js/review.js
+++ www/js/review.js
@@ -1,105 +1,43 @@
-$(document).ready(function() {
-    $('.fancy').fancybox();
+var wdgtReview = (function() {
+    var URL_PREFIX = "/upload/video/review";
 
-    $('.registration').click(function(){
-        $('#aRegistration').click();
-    });
+    var container,
+        dialog,
+        playerContainer,
+        player;
 
-    $('.CreateSales').click(function(){
-        $('#aCreateSales').click();
-    });
+    function onPreviewClick(evt) {
+        var trgt = $(evt.target).closest('.b-review-clip');
+        var title = trgt.attr('title');
 
-    $('.categoryEdit').click(function(){
-        $('#aCategoryEdit').click();
-    });
+        var index = trgt.siblings().length;
+        dialog.dialog('open').dialog('option', 'title', title);
+        playClip(index);
+    }
 
-    $('.closeOperation').click(function(){
-        $('#aCloseOperation').click();
-    });
-
-    $('.infoPanel').click(function(){
-        $('#aInfoPanel').click();
-    });
-
-    $('.createTransaction').click(function(){
-        $('#aCreateTransaction').click();
-    });
-
-    $('.expertView').click(function(){
-        $('#aExpertView').click();
-    });
-
-    $('.analise').click(function(){
-        $('#aAnalise').click();
-    });
-
-    $('.financeTarget').click(function(){
-        $('#aFinanceTarget').click();
-    });
-
-    $('.capitalCreate').click(function(){
-        $('#aCapitalCreate').click();
-    });
-
-    /*
-        $('body').mousemove(function(){
-            if (!$('.map div:hover').length)
-            {
-                $('.w_dialog.mapContent').hide();
-                for (var N=2; N<=10; N++)
-                $('.w_dialog.mapContent'+N).hide();
-            }
-           // return false;
-        })
-        //$('#w_dialog.mapContent').css('position','absolute');
-
-        //$('#w_dialog.mapContent').hide();
-
-    $('.map div').mouseover(function(){//.popup_map div
-        var c = $(this).attr('class');
-                //alert(c)
-        switch (c)
-        {
-            case 'registration':
-                            $('.w_dialog.mapContent').show();
-                break;
-            case 'CreateSales':
-                            $('.w_dialog.mapContent2').css('display','block');
-                break;
-            case 'categoryEdit':
-                            $('.w_dialog.mapContent3').css('display','block');
-                break;
-                        case 'closeOperation':
-                            $('.w_dialog.mapContent4').css('display','block');
-                break;
-                        case 'infoPanel':
-                            $('.w_dialog.mapContent5').css('display','block');
-                break;
-                         case 'createTransaction':
-                            $('.w_dialog.mapContent6').css('display','block');
-                break;
-                         case 'expertView':
-                            $('.w_dialog.mapContent7').css('display','block');
-                break;
-                         case 'analise':
-                            $('.w_dialog.mapContent8').css('display','block');
-                break;
-                         case 'financeTarget':
-                            $('.w_dialog.mapContent9').css('display','block');
-                break;
-                         case 'capitalCreate':
-                            $('.w_dialog.mapContent10').css('display','block');
-                break;
+    function playClip(index) {
+        if (!player) {
+            player = flowplayer(playerContainer.attr('id'), "/swf/flowplayer-3.1.5.swf", {
+                clip: {
+                    autoPlay: false,
+                    autoBuffering: false
+                }
+            })
         }
-                return false;
-    });
-        //$('#w_dialog.mapContent').hide();
-        $('.link.close').click(function(){
-            $('.w_dialog.mapContent').hide();
-            for (var N=1; N<=10; N++)
-            $('.w_dialog.mapContent'+N).hide();
-        });
-        */
-});
 
+        player.play(URL_PREFIX + index + '.mp4');
+    }
 
+    function init() {
+        container = $('.js-widget-review');
+
+        $('.b-review-clip-preview').live('click', onPreviewClick);
+
+        dialog = container.find('.js-review-dialog');
+        dialog.dialog({title: "Обзор", bgiframe: true, autoOpen: false, modal:true, width: 'auto'});
+
+        playerContainer = dialog.find('.js-review-playercontainer');
+    }
+
+    $(init);
+})();
