17c17defbf1ef9cfb8c58f38c436c621407ed5cb
Konstantin Kitmanov <doctor.hogart@gmail.com>
2010-10-28 11:34:39 +0400

    t2217 Контекстные ссылки на видеоролики
    * Механизм открытия видеролика, если в урле есть непустой хэш

--- www/js/review.js
+++ www/js/review.js
@@ -25,15 +25,29 @@ var wdgtReview = (function() {
         })
     }
 
+    function onHash() {
+        var clipno,
+            hash = window.location.hash.replace('#', '');
+
+        if (hash) {
+            clipno = parseInt(hash.replace('playclip', ''));
+            $('.b-review-clip-preview').eq(clipno - 1).trigger('click');
+        }
+    }
+
     function init() {
         container = $('.js-widget-review');
 
         $('.b-review-clip-preview').live('click', onPreviewClick);
 
         dialog = container.find('.js-review-dialog');
-        dialog.dialog({title: "Обзор", bgiframe: true, autoOpen: false, modal:true, width: 'auto'});
+        dialog.dialog({title: "Обзор", bgiframe: true, autoOpen: false, modal: true, width: 'auto'});
 
         playerContainer = dialog.find('.js-review-playercontainer');
+
+        if (window.location.hash) {
+            onHash();
+        }
     }
 
     $(init);
