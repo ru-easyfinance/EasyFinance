97f4d6f41573da37bbb2be1e3bbbbb27238a76a6
Konstantin Kitmanov <doctor.hogart@gmail.com>
2010-11-01 16:00:22 +0300

    t2216 Устаревшее верхнее меню на симфони
    * Вынес в общие файлы, сколько получилось
    * Актуализировал
    * почистил старые комменты, поправил отступы

---
+++ sf/apps/frontend/templates/_common/loginform.php
@@ -0,0 +1,17 @@
+<form method="post" action="https://<?php echo URL_ROOT ?>login/">
+    <div class="b-login-form">
+        <div class="b-login-form-row l-buttons-inline">
+            <?php include_partial('global/common/ui/textfield', array('name' => 'login', 'placeholder' => 'логин/почта')) ?>
+            <?php include_partial('global/common/ui/textfield', array('type' => 'password', 'name' => 'pass', 'placeholder' => 'пароль')) ?>
+            <input class="b-button-login" type="submit" value="" title="Войти"/>
+        </div>
+        <div class="b-login-form-row second">
+            <label><input type="checkbox" name="autoLogin" value="1" checked="checked" /> <span>Запомнить</span></label>
+            <a href="<?php echo URL_ROOT ?>restore/">Забыли пароль?</a>
+        </div>
+        <div class="b-login-form-bg">
+            <i class="l"></i>
+            <i class="r"></i>
+        </div>
+    </div>
+</form>
--- sf/apps/frontend/templates/layout.php
+++ sf/apps/frontend/templates/layout.php
@@ -1,6 +1,3 @@
-<?php
-#Max: давай резать шаблон на куски, чтобы удалять их из старого кода
-?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
     <head>
@@ -9,8 +6,11 @@
         <?php include_title() ?>
         <link rel="shortcut icon" href="/favicon.ico" />
         <?php include_stylesheets() ?>
-        <!--[if gte IE 7]>
+        <!--[if IE 7]>
             <link rel="stylesheet" type="text/css" href="/css/ie7.css?r=<?php echo REVISION ?>"/>
+        <![endif]-->
+        <!--[if IE 8]>
+            <link rel="stylesheet" type="text/css" href="/css/ie8.css?r=<?php echo REVISION ?>"/>
         <![endif]-->
         <?php include_javascripts() ?>
         <?php include_partial('global/res', array()) ?>
@@ -26,31 +26,16 @@
         <?php if (strpos($_SERVER['HTTP_HOST'], 'rambler') === false) : ?>
             <?php if (!$sf_user->isAuthenticated()) : ?>
                 <div id="menumain">
-                    <ul class="menu1">
-                        <!-- Jet. ticket #273 -->
-                        <li class="first"><a href="https://<?php echo URL_ROOT_MAIN; ?>review/" id="review" class="efTooltip" title="Описание основных элементов и сервисов">Обзор</a></li>
-                        <li><a href="https://<?php echo URL_ROOT_WIKI; ?>tiki-view_blog.php?blogId=2" id="feed" class="efTooltip" title="Мнения пользователей о работе сайта и их пожелания">Отзывы</a></li>
-                        <li><a href="https://<?php echo URL_ROOT_WIKI; ?>tiki-view_blog.php?blogId=4" id="feed" class="efTooltip" title="Публикации СМИ о нашем сервисе">Сми о нас</a></li>
-                        <li><a href="https://<?php echo URL_ROOT_WIKI; ?>tiki-view_blog.php?blogId=1" id="blog" class="efTooltip" title="Корпоративный блог">Блог</a></li>
-                        <li><a href="https://<?php echo URL_ROOT_WIKI; ?>tiki-view_articles.php" id="articles" title="Статьи о финансах">Статьи</a></li>
-                        <li><a href="https://<?php echo URL_ROOT_PDA; ?>" id="pda" title="Версия для использования с мобильного телефона">Мобильная версия</a></li>
-                        <li><a href="https://<?php echo URL_ROOT_WIKI; ?>tiki-read_article.php?articleId=14" id="help" style="font-weight:bold; color: white;" title="Помощь по использованию сервиса">Помощь</a></li>
-                        <li style="background: none; margin-left: -1em;"><a href="https://{$smarty.const.URL_ROOT_MAIN}integration" style="font-weight:bold; color: yellow;" title="Интеграция с банками">EasyBank</a></li>
-                    </ul>
-                    <ul class="menu2">
-                        <?php
-                        /*
-                        <!--- <li><a href="/info/">Личный кабинет</a></li> --->
-                        <!--- <li><a href="/profile/">Настройки профиля</a></li> --->
-                        */
-                        ?>
-                        <?php if ($sf_user->isAuthenticated()) : ?>
-                        <li><a href="/my/profile/"><?php echo $sf_user->getName(); ?></a></li>
-                        <li><a id="show_logout" href="/logout/" title="Выход">ВЫХОД</a></li>
-                        <?php else: ?>
-                        <li id="show_login"><a id="linkLogin" href="https://<?php echo URL_ROOT ?>login/">ВХОД</a></li>
-                        <?php endif; ?>
-                    </ul>
+                    <?php include_partial('global/common/uppermenu', array()) ?>
+
+                    <?php if ($sf_user->isAuthenticated()) : ?>
+                        <ul class="menu2">
+                            <li><a href="/my/profile/"><?php echo $sf_user->getName(); ?></a><a id="show_logout" href="/logout/" title="Выход">ВЫХОД</a></li>
+                        </ul>
+                    <?php else: ?>
+                        <?php include_partial('global/common/loginform', array()) ?>
+                    <?php endif; ?>
+
                 </div>
             <?php endif?>
 <!-- верхнее меню -->
@@ -69,7 +54,7 @@
 <div id="mainwrap"  style="z-index: 5">
     <!-- mainmenu -->
     <?php if ($sf_user->isAuthenticated()) : ?>
-    <?php include_partial('global/common/mainmenu'); ?>
+        <?php include_partial('global/common/mainmenu'); ?>
     <?php endif; ?>
     <div class="ramka2">
         <div class="cct">
@@ -78,8 +63,6 @@
         </div>
         <div class="ccm">
             <div class="ccm-container">
-                <!--меню4-->
-                <!--/меню4-->
                 <div class="mid general_cont">
                     <?php if (isset($mainBlockClear)) : ?>
                         <?php include '_mainBlockClear.php'; ?>
