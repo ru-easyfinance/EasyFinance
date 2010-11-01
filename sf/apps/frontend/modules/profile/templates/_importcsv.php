2d9ced6764279eb16c5d72d18c42684a42258aa6
Konstantin Kitmanov <doctor.hogart@gmail.com>
2010-11-01 18:52:56 +0300

    t2195 Импорт CSV: клиент -- правки
    * изменение состояния доступности кнопки отправки формы на время
    * отправки запроса и получения ответа от сервера

--- sf/apps/frontend/modules/profile/templates/_importcsv.php
+++ sf/apps/frontend/modules/profile/templates/_importcsv.php
@@ -1,7 +1,7 @@
 <div class="inside form">
     <div class="js-widget js-widget-importcsv b-importcsv">
         <p>Импорт данных из других сервисов в формате CSV</p>
-        <form class="js-control js-control-ajaxupload b-form-skeleton b-importcsv-form" action="/my/profile/import_csv" method="POST">
+        <form class="b-form-skeleton b-importcsv-form" action="/my/profile/import_csv" method="POST">
             <div class="b-row">
                 <div class="b-col">
                     <div class="b-col-indent">
--- www/js/wdgt-importcsv.js
+++ www/js/wdgt-importcsv.js
@@ -6,17 +6,36 @@ var importcsv = function (element, doc, dispatcher) {
     // mixin the widget here
 
     this.__init__();
-}
+};
 
 importcsv.prototype = {
+    frm: null,
+    submit: null,
+
     __init__: function() {
-        utils.initControls(this.element);
+        var that = this;
+
+        this.frm = this.element.find('form');
+        this.submit = this.frm.find('input[type="submit"]');
+
+        this.frm.bind('submit', function(evt) { that.onSubmit.apply(that, [evt]) });
+    },
+    onSubmit: function(evt) {
+        evt.preventDefault();
+        this.submit.attr('disabled', 'disabled');
+
+        function onResponse(data) {
+            utils.defaultOnSuccess(data);
+            this.submit.removeAttr('disabled');
+        }
+
+        utils.ajaxUpload(this.frm, onResponse, onResponse, this);
     }
-}
+};
 
 $(function(){
     var uploadFrm = $('.js-widget-importcsv');
     if (uploadFrm.length) {
         new importcsv(uploadFrm, $(document), null)
     }
-})
+});
