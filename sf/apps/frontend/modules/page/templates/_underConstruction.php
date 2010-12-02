ca954937523a5ef4a9f2bef28bff22fea5629cca
Anton Minin <anton.a.minin@gmail.com>
2010-12-02 15:35:58 +0300

    t2328 Скрыть страницу сити-банка

---
+++ sf/apps/frontend/modules/page/templates/_underConstruction.php
@@ -0,0 +1,5 @@
+<div style="width:600px; height: 400px; position:relative; margin: auto; padding: 1px; background: url(/img/i/under-construction-helmet.png) top left no-repeat;">
+    <div style="width: 480px; margin: 200px auto 0px auto; font-size: 25px; text-align:center;">
+        Раздел находится в разработке.
+    </div>
+</div>
