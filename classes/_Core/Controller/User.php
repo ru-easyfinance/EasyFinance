e8f5fbd5ab2932e6a0a9948346dfa8f4d8cdaf40
Anton Belousov <anton.n.belousov@gmail.com>
2010-12-03 00:35:42 +0300

    t0958: used newly created Coree:CurrentUserIsAuthorized instead of checking user id

--- classes/_Core/Controller/User.php
+++ classes/_Core/Controller/User.php
@@ -5,7 +5,7 @@ abstract class _Core_Controller_User extends _Core_Controller
     public function __construct( $templateEngine, _Core_Request $request)
     {
         //Проверяем авторизован ли пользователь. Если нет - редиректим на логин
-        if( !Core::getInstance()->user->getId() )
+        if( !Core::getInstance()->CurrentUserIsAuthorized())
         {
             header( 'Location: /login/' );
         }
